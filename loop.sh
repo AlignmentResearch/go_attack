e3fde75f80d8d49119b9df1d1be8cad59481fc2c
ttw <1733389+ed1d1a8d@users.noreply.github.com>
2022-04-30 19:35:14 -0400

    Tabs -> spaces

--- loop.sh
+++ loop.sh
@@ -2,15 +2,15 @@
 
 docker-compose --version
 for i in {0..100}; do
-	echo "*** Iteration ${i} ***"
-
-	output_dir=bug-repro-logs/run-${i}
-	mkdir -p ${output_dir}
+    echo "*** Iteration ${i} ***"
+    
+    output_dir=bug-repro-logs/run-${i}
+    mkdir -p ${output_dir}
     mv bug-repro-logs/active bug-repro-logs/active.bak >/dev/null 2>&1
-	ln -s run-${i} bug-repro-logs/active
+    ln -s run-${i} bug-repro-logs/active
 
-	echo "Starting Docker compose"
-	docker-compose \
+    echo "Starting Docker compose"
+    docker-compose \
         -f compose/crash.yml \
         --env-file compose/crash.env \
         up \
@@ -18,7 +18,7 @@ for i in {0..100}; do
         2>${output_dir}/compose.stderr&
 
     wait_time=45
-	echo "Waiting for ${wait_time} seconds"
+    echo "Waiting for ${wait_time} seconds"
     for i in $(seq $wait_time)
     do
         echo -n .
@@ -26,8 +26,8 @@ for i in {0..100}; do
     done
     echo -e "\nDone waiting."
 
-	echo "Trying to bring docker service down now."
-	echo "If this hangs, then bug detected!"
-	docker-compose -f compose/crash.yml --env-file compose/crash.env down
-	wait
+    echo "Trying to bring docker service down now."
+    echo "If this hangs, then bug detected!"
+    docker-compose -f compose/crash.yml --env-file compose/crash.env down
+    wait
 done
