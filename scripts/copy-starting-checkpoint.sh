fc528f9ddd20a9f6426513e03976cd17d3b3fb54
Tony Wang <twang6@mit.edu>
2022-08-08 20:19:09 -0700

    Remove links (which don't work in docker)
    Also copy over config

--- scripts/copy-starting-checkpoint.sh
+++ scripts/copy-starting-checkpoint.sh
@@ -7,15 +7,18 @@ CKPT_DIR="/nas/ucb/tony/go-attack/checkpoints/katagotraining.org/b20c256x2-s5303
 INIT_WEIGHT_DIR="$EXP_ROOT/train/t0/initial_weights"
 mkdir -p "$INIT_WEIGHT_DIR"
 
+cp -v -- "$CKPT_DIR/saved_model/model.config.json" "$EXP_ROOT/train/t0/"
+
 VAR_DIR="$CKPT_DIR/saved_model/variables"
 pushd $VAR_DIR
 for f in *; do
-    ln -vsf -- "$VAR_DIR/$f" "$INIT_WEIGHT_DIR/model.$f";
+    cp -v -- "$VAR_DIR/$f" "$INIT_WEIGHT_DIR/model.$f";
 done
 popd
 ##############################################################################
 
 ######################### Copy files into models #############################
 mkdir -p "$EXP_ROOT/models"
-ln -vsf "$CKPT_DIR" "$EXP_ROOT/models/t0-s0-d0"
+rm -rf "$EXP_ROOT/models/t0-s0-d0"
+cp -rv -- "$CKPT_DIR" "$EXP_ROOT/models/t0-s0-d0"
 ##############################################################################
