c9ec40dbc44ac9f2b1b060ce7fb46736b0f7c46e
Adam Gleave <adam@gleave.me>
2022-08-23 15:33:19 -0700

    Refactor baseline attack into helper methods

--- src/go_attack/baseline_attack.py
+++ src/go_attack/baseline_attack.py
@@ -3,12 +3,13 @@ import random
 import re
 from pathlib import Path
 from subprocess import DEVNULL, PIPE, Popen
-from typing import List, Literal, Optional
+from typing import IO, AnyStr, List, Literal, Optional, Tuple
 
 from tqdm import tqdm
 
 from go_attack.adversarial_policy import (
     POLICIES,
+    AdversarialPolicy,
     MyopicWhiteBoxPolicy,
     NonmyopicWhiteBoxPolicy,
     PassingWrapper,
@@ -26,38 +27,15 @@ PASSING_BEHAVIOR = (
 )
 
 
-def run_baseline_attack(
+def start_engine(
+    executable_path: Path,
+    config_path: Path,
     model_path: Path,
-    adversarial_policy: str,
     num_playouts: int,
     passing_behavior: str,
-    gpu: Optional[int] = None,
-    *,
-    board_size: int = 19,
-    config_path: Path,
-    executable_path: Path,
-    log_analysis: bool = False,
-    log_dir: Optional[Path] = None,
-    moves_before_pass: int = 211,
-    num_games: int = 1,
-    progress_bar: bool = True,
-    seed: int = 42,
-    verbose: bool = False,
-    victim: Literal["B", "W"] = "B",
-) -> List[Game]:
-    """Run a baseline attack against KataGo."""
-    if adversarial_policy not in POLICIES:
-        raise ValueError(f"adversarial_policy must be one of {POLICIES}")
-    if passing_behavior not in PASSING_BEHAVIOR:
-        raise ValueError(f"passing_policy must be one of {PASSING_BEHAVIOR}")
-    if not config_path.exists():
-        raise ValueError(f"config_path must exist: {config_path}")
-    if not model_path.exists():
-        raise ValueError(f"model_path must exist: {model_path}")
-    if gpu is None:
-        gpu = select_best_gpu(10)
-
-    # Start up the KataGo executable.
+    gpu: int,
+) -> Tuple[IO[AnyStr], IO[AnyStr]]:
+    """Starts the KataGo engine, returning a tuple with the engines stdin and stdout."""
     proc = Popen(
         [
             str(executable_path),
@@ -83,42 +61,117 @@ def run_baseline_attack(
     stdin = proc.stdin
     stdout = proc.stdout
     assert stdin is not None and stdout is not None
+    return stdin, stdout
 
-    analysis_regex = re.compile(r"info.*|play pass")
-    analysis_move_regex = re.compile(r"play ([A-Z][0-9]{1,2}|pass)")
-    move_regex = re.compile(r"= ([A-Z][0-9]{1,2}|pass)")
-    score_regex = re.compile(r"= (B|W)\+([0-9]+\.?[0-9]*)|= 0")
 
-    def get_msg(pattern):
-        while True:
-            msg = stdout.readline().decode("ascii").strip()
-            if msg == "? genmove returned null locati on or illegal move":
-                print("Internal KataGo error; board state:")
-                print_kata_board()
-            elif hit := pattern.fullmatch(msg):
-                return hit
+def make_log_dir(
+    log_root: Path,
+    model_path: Path,
+    adversarial_policy: str,
+    num_playouts: int,
+    passing_behavior: str,
+) -> Path:
+    """Make a log directory and return the Path to it."""
+    desc = f"model={model_path.stem}"
+    desc += f"_policy={adversarial_policy}"
+    desc += f"_playouts={num_playouts}"
+    desc += f"_pass={passing_behavior}"
+
+    log_dir = log_root / desc
+    log_dir.mkdir(exist_ok=True, parents=True)
+    print(f"Logging SGF game files to '{str(log_dir)}'")
+    return log_dir
+
+
+def run_baseline_attack(
+    model_path: Path,
+    adversarial_policy: str,
+    num_playouts: int,
+    passing_behavior: str,
+    gpu: Optional[int] = None,
+    *,
+    board_size: int = 19,
+    config_path: Path,
+    executable_path: Path,
+    log_analysis: bool = False,
+    log_root: Optional[Path] = None,
+    moves_before_pass: int = 211,
+    num_games: int = 1,
+    progress_bar: bool = True,
+    seed: int = 42,
+    verbose: bool = False,
+    victim: Literal["B", "W"] = "B",
+) -> List[Game]:
+    """Run a baseline attack against KataGo."""
+    if adversarial_policy not in POLICIES:
+        raise ValueError(f"adversarial_policy must be one of {POLICIES}")
+    if passing_behavior not in PASSING_BEHAVIOR:
+        raise ValueError(f"passing_policy must be one of {PASSING_BEHAVIOR}")
+    if not config_path.exists():
+        raise ValueError(f"config_path must exist: {config_path}")
+    if not model_path.exists():
+        raise ValueError(f"model_path must exist: {model_path}")
+    if gpu is None:
+        gpu = select_best_gpu(10)
+
+    # Start up the KataGo executable.
+    to_engine, from_engine = start_engine(
+        executable_path,
+        config_path,
+        model_path,
+        num_playouts,
+        passing_behavior,
+        gpu,
+    )
+
+    log_dir = None
+    if log_root is not None:
+        log_dir = make_log_dir(
+            log_root,
+            model_path,
+            adversarial_policy,
+            num_playouts,
+            passing_behavior,
+        )
 
     def maybe_print(msg):
         if verbose:
             print(msg)
 
     def send_msg(msg):
-        stdin.write(f"{msg}\n".encode("ascii"))
+        to_engine.write(f"{msg}\n".encode("ascii"))
+
+    def print_kata_board():
+        send_msg("showboard")
+        for _ in range(board_size + 3):
+            msg = from_engine.readline().decode("ascii").strip()
+            print(msg)
+
+    def get_msg(pattern):
+        while True:
+            msg = from_engine.readline().decode("ascii").strip()
+            if msg == "? genmove returned null locati on or illegal move":
+                print("Internal KataGo error; board state:")
+                print_kata_board()
+            elif hit := pattern.fullmatch(msg):
+                return hit
+
+    def make_policy() -> AdversarialPolicy:
+        if policy_cls in (MyopicWhiteBoxPolicy, NonmyopicWhiteBoxPolicy):
+            policy = policy_cls(
+                game,
+                victim_color.opponent(),
+                to_engine,
+                from_engine,
+            )  # pytype: disable=not-instantiable,wrong-arg-count
+        else:
+            policy = policy_cls(
+                game,
+                victim_color.opponent(),
+            )  # pytype: disable=not-instantiable
+        return PassingWrapper(policy, moves_before_pass)
 
     send_msg(f"boardsize {board_size}")
-    if log_dir:
-        desc = f"model={model_path.stem}"
-        desc += f"_policy={adversarial_policy}"
-        desc += f"_playouts={num_playouts}"
-        desc += f"_pass={passing_behavior}"
-
-        log_dir = log_dir / desc
-        log_dir.mkdir(exist_ok=True, parents=True)
-        print(f"Logging SGF game files to '{str(log_dir)}'")
-
-    game_iter = range(num_games)
-    if not verbose and progress_bar:
-        game_iter = tqdm(game_iter, desc="Playing", unit="games")
 
     random.seed(seed)
     games = []
@@ -126,27 +179,20 @@ def run_baseline_attack(
     victim_color = Color.from_str(victim)
     victim_name = "Black" if victim == "B" else "White"
 
+    analysis_regex = re.compile(r"info.*|play pass")
+    analysis_move_regex = re.compile(r"play ([A-Z][0-9]{1,2}|pass)")
+    move_regex = re.compile(r"= ([A-Z][0-9]{1,2}|pass)")
+    score_regex = re.compile(r"= (B|W)\+([0-9]+\.?[0-9]*)|= 0")
+
+    game_iter = range(num_games)
+    if not verbose and progress_bar:
+        game_iter = tqdm(game_iter, desc="Playing", unit="games")
+
     for i in game_iter:
         maybe_print(f"\n--- Game {i + 1} of {num_games} ---")
         game = Game(board_size)
 
-        # Add comment to the SGF file
-        strat_title = adversarial_policy.capitalize()
-
-        if policy_cls in (MyopicWhiteBoxPolicy, NonmyopicWhiteBoxPolicy):
-            policy = policy_cls(
-                game,
-                victim_color.opponent(),
-                stdin,
-                stdout,
-            )  # pytype: disable=not-instantiable,wrong-arg-count
-        else:
-            policy = policy_cls(
-                game,
-                victim_color.opponent(),
-            )  # pytype: disable=not-instantiable
-
-        policy = PassingWrapper(policy, moves_before_pass)
+        policy = make_policy()
 
         def take_turn():
             move = policy.next_move()
@@ -155,12 +201,6 @@ def run_baseline_attack(
             vertex = str(move) if move else "pass"
             send_msg(f"play {victim_color.opponent()} {vertex}")
             maybe_print("Passing" if move is None else f"Playing {vertex}")
-
-        def print_kata_board():
-            send_msg("showboard")
-            for _ in range(board_size + 3):
-                msg = stdout.readline().decode("ascii").strip()
-                print(msg)
 
         # Play first iff we're black
         if victim_color.opponent() == Color.BLACK:
@@ -229,6 +269,7 @@ def run_baseline_attack(
 
         # Save the game to disk if necessary
         if log_dir:
+            strat_title = adversarial_policy.capitalize()
             sgf = game.to_sgf(f"{strat_title} attack; {victim_name} victim")
 
             with open(log_dir / f"game_{i}.sgf", "w") as f:
