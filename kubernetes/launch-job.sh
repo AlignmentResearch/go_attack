2e68c782b9c803f60f2d622cbb14b32bcda1b65c
Nora Belrose <belrose.nora@gmail.com>
2022-08-26 11:38:51 -0700

    Correct number of threads per worker

--- kubernetes/launch-job.sh
+++ kubernetes/launch-job.sh
@@ -4,19 +4,19 @@ if [ $# -lt 1 ]; then
     exit 2
 fi
 
-VICTIM=${2:-/shared/victims/kata1-b20c256x2-s5303129600-d1228401921.bin.gz}
+VICTIM=${2:-/shared/victims/kata1-b6c96-s41312768-d6061202.txt.gz}
 
 ctl job run --container \
     humancompatibleai/goattack:2022-08-25-v2 \
     humancompatibleai/goattack:2022-08-25-v2 \
-    humancompatibleai/goattack:2022-08-22-v2-python \
-    humancompatibleai/goattack:2022-08-22-v2-python \
+    humancompatibleai/goattack:2022-08-26-python-v3 \
+    humancompatibleai/goattack:2022-08-26-python-v3 \
     --volume_name go-attack \
     --volume_mount shared \
     --command "/shared/kubernetes/victimplay.sh $1 $VICTIM" \
     "python3 /shared/kubernetes/evaluate_loop.py $1 $VICTIM" \
     "/shared/kubernetes/train.sh $1" \
     "/shared/kubernetes/shuffle-and-export.sh $1" \
-    --gpu 1 1 0 1 \
+    --gpu 1 1 1 0 \
     --name go-training \
     --replicas "${3:-7}" 1 1 1
