8799d9412fafeeaedc34c82283964c968317bd47
Tom Tseng <tomtseng@users.noreply.github.com>
2022-08-30 10:52:52 -0400

    bench-setup: gen key, install gpustat

--- bench-setup.sh
+++ bench-setup.sh
@@ -3,3 +3,7 @@ mkdir --parents training/emcts1-v2/cp127-vis512-warmstart-vis32
 wget --directory-prefix=victim-models/ https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b40c256-s11840935168-d2898845681.bin.gz
 sudo usermod -aG docker $USER
 sudo service docker start
+ssh-keygen -t ed25519 -C "tom.hm.tseng@gmail.com"
+cat /home/t/.ssh/id_ed25519.pub
+sudo apt install python3-pip
+pip3 install gpustat
