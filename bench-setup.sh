50464da66dddf0adea786928934e7b3a02d29bd5
Tom Tseng <tomtseng@users.noreply.github.com>
2022-08-30 10:21:04 -0400

    bench-setup: start docker

--- bench-setup.sh
+++ bench-setup.sh
@@ -1,3 +1,5 @@
 mkdir victim-models
 mkdir --parents training/emcts1-v2/cp127-vis512-warmstart-vis32
 wget --directory-prefix=victim-models/ https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b40c256-s11840935168-d2898845681.bin.gz
+sudo usermod -aG docker $USER
+sudo service docker start
