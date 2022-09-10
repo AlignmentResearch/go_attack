2344db94dd6e2f41a97adf20dfcf7eff599d3e70
Tony Wang <twang6@mit.edu>
2022-09-10 08:39:24 -0700

    Set up matches/cp505-adv-visit-vary experiment

--- scripts/gen_match_cfg.py
+++ scripts/gen_match_cfg.py
@@ -13,9 +13,15 @@ class Bot:
     max_visits: int
 
 
-MODELS_WITH_RANKS = [
-    ("kata1-b40c256-s11840935168-d2898845681.bin.gz", "-cp505"),
-    ("kata1-b20c256x2-s5303129600-d1228401921.bin.gz", "-cp127"),
+MODELS_WITH_NAMES = [
+    (
+        "/garoot/training/emcts1-curr/cp127-to-505-v1/models/t0-s34090496-d8262123/model.bin.gz",
+        "adv-s34090496",
+    ),
+    (
+        "/garoot/training/emcts1-curr/cp127-to-505-v1/models/t0-s59042304-d14514443/model.bin.gz",
+        "adv-s59042304",
+    ),
 ]
 
 N_GPUS = 8
@@ -27,13 +33,13 @@ def main():
     bots: List[Bot] = []
 
     # Generate bot parameters
-    for nn_file, rank in MODELS_WITH_RANKS:
-        for vpow in range(10):
+    for nn_file, name in MODELS_WITH_NAMES:
+        for visits in sorted(list(set([2**_ for _ in range(11)] + [600]))):
             bots.append(
                 Bot(
-                    nickname=f"bot{rank}-v{2**vpow}",
+                    nickname=f"{name}-v{visits}",
                     nn_file=nn_file,
-                    max_visits=2**vpow,
+                    max_visits=visits,
                 ),
             )
 
@@ -41,13 +47,16 @@ def main():
     print(f"numBots = {len(bots)}")
     print()
     for idx, bot in enumerate(bots):
-        print(f"nnModelFile{idx} = /models/{bot.nn_file}")
+        print(f"nnModelFile{idx} = {bot.nn_file}")
         print(f"botName{idx} = {bot.nickname}")
         print(f"maxVisits{idx} = {bot.max_visits}")
+        print(f"searchAlgorithm{idx} = EMCTS1")
+        print(f"EMCTS1_noiseOppNodes{idx} = true")
+        print()
     print()
 
     assert N_THREADS_PER_MODEL % N_GPUS == 0
-    for model_idx, _ in enumerate(MODELS_WITH_RANKS):
+    for model_idx, _ in enumerate(MODELS_WITH_NAMES):
         for thread_idx in range(N_THREADS_PER_MODEL):
             gpu_idx = thread_idx % N_GPUS
             print(f"cudaDeviceToUseModel{model_idx}Thread{thread_idx} = {gpu_idx}")
