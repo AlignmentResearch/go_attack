95b5b0e7cf1681dc7bb4f110b182154ea5d29cb9
Tom Tseng <ttseng@vae.ist.berkeley.edu>
2022-09-13 23:45:52 +0000

    remove double elf launch
    polling the victim's port with `nc -z` causes the victim container's socat to
    create an extra instance of the victim

--- compose/transfer/connect-to-victim.sh
+++ compose/transfer/connect-to-victim.sh
@@ -2,13 +2,10 @@
 # Connects to a Docker container named `victim` on port 80. This is used for
 # running transfer experiments where the victim is run on another container and
 # exposed on a port.
+# The port should already be exposed on the victim's container or else netcat
+# will terminate immediately.
 
 CONTAINER_NAME="victim"
 PORT=80
-
-while ! nc -z ${CONTAINER_NAME} ${PORT} ; do
-  echo "Waiting for port ${PORT}..."
-  sleep 1
-done
 
 nc ${CONTAINER_NAME} ${PORT}
