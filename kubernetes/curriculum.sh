c27e251e29fe51f5e6de56f6ea329c7d021b35cf
Nora Belrose <belrose.nora@gmail.com>
2022-09-20 11:53:03 -0700

    Trying to get K8 to work properly

--- kubernetes/curriculum.sh
+++ kubernetes/curriculum.sh
@@ -3,4 +3,4 @@ python /engines/KataGo-custom/python/curriculum.py \
     -selfplay-dir=/shared/victimplay/"$1"/selfplay/ \
     -input-models-dir=/shared/victims \
     -output-models-dir=/shared/victimplay/"$1"/victims \
-    -config-json-file=/shared/configs/curriculum.json
+    -config-json-file=/shared/configs/curriculum-cp37.json
--- kubernetes/victimplay.sh
+++ kubernetes/victimplay.sh
@@ -3,4 +3,5 @@
     -output-dir /shared/victimplay/"$1"/selfplay/ \
     -models-dir /shared/models/ \
     -nn-victim-path /shared/victims/ \
-    -config /shared/configs/active-experiment.cfg
+    -config /shared/configs/active-experiment.cfg \
+    -config /shared/configs/compute/1gpu.cfg
