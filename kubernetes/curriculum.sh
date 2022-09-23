09be24db5884c0d19bb7302f55ee8e377691e793
Nora Belrose <belrose.nora@gmail.com>
2022-09-22 17:24:26 -0700

    Remove -p from mkdir

--- kubernetes/curriculum.sh
+++ kubernetes/curriculum.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 RUN_NAME="$1"
 VOLUME_NAME="$2"
-mkdir -p /"$VOLUME_NAME"/victimplay/"$RUN_NAME"
+mkdir /"$VOLUME_NAME"/victimplay/"$RUN_NAME"
 python /engines/KataGo-custom/python/curriculum.py \
     -selfplay-dir=/"$VOLUME_NAME"/victimplay/"$RUN_NAME"/selfplay/ \
     -input-models-dir=/"$VOLUME_NAME"/victims \
--- kubernetes/victimplay.sh
+++ kubernetes/victimplay.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 RUN_NAME="$1"
 VOLUME_NAME="$2"
-mkdir -p /"$VOLUME_NAME"/victimplay/"$RUN_NAME"
+mkdir /"$VOLUME_NAME"/victimplay/"$RUN_NAME"
 /engines/KataGo-custom/cpp/katago victimplay \
     -output-dir /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/selfplay/ \
     -models-dir /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/models/ \
