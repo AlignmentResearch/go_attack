612727a926eefb1f048f1f32b96d7505678e82d1
Nora Belrose <belrose.nora@gmail.com>
2022-09-23 13:09:34 -0700

    Add -p flag to the mkdirs again

--- kubernetes/shuffle-and-export.sh
+++ kubernetes/shuffle-and-export.sh
@@ -3,6 +3,6 @@ cd /engines/KataGo-custom/python || exit
 RUN_NAME="$1"
 DIRECTORY="$2"
 VOLUME_NAME="$3"
-mkdir /"$VOLUME_NAME"/victimplay/"$DIRECTORY"
+mkdir -p /"$VOLUME_NAME"/victimplay/"$DIRECTORY"
 ./selfplay/shuffle_and_export_loop.sh    "$RUN_NAME"    /"$VOLUME_NAME"/victimplay/"$DIRECTORY"    /tmp    16    256    0
 sleep infinity
