c01d859189b429c45e6593f26e9478f00d851009
Tom Tseng <tom.hm.tseng@gmail.com>
2022-09-23 20:23:25 +0000

    launch.sh: add timestamp (up to seconds) into output dir name

--- compose/transfer/launch.sh
+++ compose/transfer/launch.sh
@@ -44,15 +44,15 @@ DOCKER_COMPOSE_COMMAND=$2
 
 # Exported variables like these are used inside the docker-compose yml file.
 export HOST_REPO_ROOT=$(git rev-parse --show-toplevel)
-export HOST_OUTPUT_DIR=${HOST_REPO_ROOT}/transfer-logs/${EXPERIMENT_NAME}
+export HOST_OUTPUT_DIR=${HOST_REPO_ROOT}/transfer-logs/${EXPERIMENT_NAME}/$(date +%Y%m%d-%H%M%S)
 mkdir --parents ${HOST_OUTPUT_DIR}
 if [ ${EXPERIMENT_NAME} = "baseline-attack-vs-leela" ]; then
   export HOST_LEELA_TUNING_FILE=${HOST_REPO_ROOT}/engines/leela/leelaz_opencl_tuning
   touch -a ${HOST_LEELA_TUNING_FILE}
 fi
 
-docker-compose \
-  --file ${SCRIPT_DIR}/${EXPERIMENT_NAME}.yml \
-  --env-file ${ENV_FILE} \
-  --project-name ${EXPERIMENT_NAME} \
-  ${DOCKER_COMPOSE_COMMAND}
+# docker-compose \
+#   --file ${SCRIPT_DIR}/${EXPERIMENT_NAME}.yml \
+#   --env-file ${ENV_FILE} \
+#   --project-name ${EXPERIMENT_NAME} \
+#   ${DOCKER_COMPOSE_COMMAND}
