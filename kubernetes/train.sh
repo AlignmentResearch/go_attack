ddd8a515dac0bd65b149513c4943bdee7c29209b
Nora Belrose <belrose.nora@gmail.com>
2022-09-27 12:21:13 -0700

    Disable vtimeloss & extend curriculum.json

--- kubernetes/train.sh
+++ kubernetes/train.sh
@@ -3,4 +3,4 @@ cd /engines/KataGo-custom/python || exit
 RUN_NAME="$1"
 VOLUME_NAME="$2"
 mkdir -p /"$VOLUME_NAME"/victimplay/"$RUN_NAME"
-./selfplay/train.sh    /"$VOLUME_NAME"/victimplay/"$RUN_NAME"    t0    b6c96    256    main    -lr-scale 1.0    -max-train-bucket-per-new-data 4
+./selfplay/train.sh    /"$VOLUME_NAME"/victimplay/"$RUN_NAME"    t0    b6c96    256    main    -disable-vtimeloss    -lr-scale 1.0    -max-train-bucket-per-new-data 4
