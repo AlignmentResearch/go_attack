7097352323a4a8b5b9c106590c0b8996ef7e98fe
Tony Wang <twang6@mit.edu>
2022-09-27 22:55:42 -0700

    Set up matches/cp505-v64-vs-adv-1-to-8192

--- scripts/gen_match_cfg.py
+++ scripts/gen_match_cfg.py
@@ -13,9 +13,11 @@ class Bot:
     max_visits: int
 
 
+#
 MODELS_WITH_RANKS = [
     # ("kata1-b15c192-s1503689216-d402723070.txt.gz", "cp103"),
-    ("kata1-b40c256-s11840935168-d2898845681.bin.gz", "cp505"),
+    # ("kata1-b40c256-s11840935168-d2898845681.bin.gz", "cp505"),
+    ("/garoot/training/emcts1-curr/cp127-to-505-v1/models/t0-s34090496-d8262123/model.bin.gz", "adv"),
     # ("kata1-b20c256x2-s5303129600-d1228401921.bin.gz", "cp127"),
 ]
 
@@ -29,7 +31,7 @@ def main():
 
     # Generate bot parameters
     for nn_file, bot_name in MODELS_WITH_RANKS:
-        for vpow in range(12):
+        for vpow in range(14):
             bots.append(
                 Bot(
                     nickname=f"{bot_name}-v{2**vpow}",
@@ -42,10 +44,12 @@ def main():
     print(f"numBots = {len(bots)}")
     print()
     for idx, bot in enumerate(bots):
-        print(f"nnModelFile{idx} = /models/{bot.nn_file}")
+        print(f"nnModelFile{idx} = {bot.nn_file}")
         print(f"botName{idx} = {bot.nickname}")
         print(f"maxVisits{idx} = {bot.max_visits}")
-        print(f"searchAlgorithm{idx} = MCTS")
+        print(f"searchAlgorithm{idx} = EMCTS1")
+        print(f"oppVisitsOverride{idx} = 1")
+        print(f"oppRootSymmetriesOverride{idx} = 1")
         print()
     print()
 
