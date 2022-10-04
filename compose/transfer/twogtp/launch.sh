3fa62da347c38d6bf15de1e0743cc5a7d3c5037e
Tom Tseng <tom.hm.tseng@gmail.com>
2022-10-03 21:51:32 -0700

    twogtp: basic launch.sh works

--- compose/transfer/twogtp/launch.sh
+++ compose/transfer/twogtp/launch.sh
@@ -1,5 +1,3 @@
-BLACK_COMMAND="docker run --gpus \"device=0\" -v /home/${USER}/go_attack:/go_attack -t humancompatibleai/goattack:cpp /engines/KataGo-custom/cpp/katago gtp -config /go_attack/configs/gtp.cfg  -model /dev/null -victim-model /dev/null"
-WHITE_COMMAND=${BLACK_COMMAND}
 NUM_GAMES=1
 OUTPUT_DIR=/tmp
 
@@ -9,15 +7,16 @@ HOST_DOCKER_SOCKET=/run/user/${UID}/docker.sock
 # Handles case where Docker is run not in rootless mode.
 [ ! -e ${HOST_DOCKER_SOCKET} ] && HOST_DOCKER_SOCKET=/var/run/docker.sock
 
+# We start the GTP instances first and have gogui-twogtp attach to them. If
+# gogui-twogtp started the GTP instances, then gogui-twogtp would start feeding
+# commands to them before they were ready.
+BLACK_ID=$(docker run --detach --interactive --gpus \"device=0\" --volume /home/${USER}/go_attack:/go_attack humancompatibleai/goattack:cpp /engines/KataGo-custom/cpp/katago gtp -config /go_attack/configs/gtp.cfg  -model /dev/null -victim-model /dev/null)
+WHITE_ID=$(docker run --detach --interactive --gpus \"device=0\" --volume /home/${USER}/go_attack:/go_attack humancompatibleai/goattack:cpp /engines/KataGo-custom/cpp/katago gtp -config /go_attack/configs/gtp.cfg  -model /dev/null -victim-model /dev/null)
+
 docker run -v ${HOST_DOCKER_SOCKET}:/var/run/docker.sock \
-  humancompatibleai/goattack:twogtp \
-  bin/gogui-twogtp -black "${BLACK_COMMAND}" -white "${WHITE_COMMAND}" \
+  humancompatibleai/goattack:twogtp bin/gogui-twogtp \
+  -black "docker attach ${BLACK_ID}" -white "docker attach ${WHITE_ID}" \
   -alternate -auto -games ${NUM_GAMES} -komi 6.5 -sgffile ${OUTPUT_DIR}/ \
   -verbose
 
-# I think twogtp is sending the "protocol_version" message while docker is still
-# launching and katago isn't ready? katago never sees the msg so twogtp just
-# hangs on the first message
-# TODO(tomtseng) let's try this: first launch the katago containers. then
-# gogui-gtp's -black/white command will consist of attaching to the containers?
-
+docker rm --force ${BLACK_ID} ${WHITE_ID}
