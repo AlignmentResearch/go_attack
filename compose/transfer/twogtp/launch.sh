be95a7048dc5e901356b3385af1edb547f6905cc
Tom Tseng <tom.hm.tseng@gmail.com>
2022-10-05 10:42:39 -0700

    twogtp: todos

--- compose/transfer/twogtp/launch.sh
+++ compose/transfer/twogtp/launch.sh
@@ -1,10 +1,3 @@
-# command for katago:
-#   docker run --detach --interactive --gpus \"device=0\" --volume /home/${USER}/go_attack:/go_attack humancompatibleai/goattack:cpp /engines/KataGo-custom/cpp/katago gtp -config /go_attack/configs/gtp.cfg  -model /dev/null -victim-model /dev/null
-# command for leela:
-#   docker run --detach --interactive --gpus \"device=0\" --volume <tuning file> humancompatibleai/goattack:leela ./run-gtp.sh
-# command for elf:
-#   docker run --detach --interactive --gpus \"device=0\" humancompatibleai/goattack:elf ./run-gtp.sh
-#
 # ok so i guess i should write a command line utility out of this?
 #   args:
 #     - gpu (i think it's ok to just specify one, eh)
@@ -17,14 +10,35 @@
 #   katago config
 #   output-dir
 #
-#  TODO(tomtseng) forgot about elf not allowing suicide --- can i toggle this in gtp.cfg?
-#    I can turn off multi-stone suicide but i'm not sure that's the same thing
-#    --- maybe check logic in katago's board.cpp
+#   remember to set multiStoneSuicideLegal = false
 #
-#  TODO(tomtseng) investigate: twogtp's -alternate flag docs: "Black and White
+#  TODO(tomtseng) print warning about -alternate: "Black and White
 #  are exchanged every odd game; the scores saved in the results table -sgffile
 #  are still using the name Black and White as given with -black and -white."
-#  Need to check SGF files to make sure I can figure out who is who
+# TODO probably noponder on leelaz for consistency
+#
+# TODO one serious issue is that each game takes so long to run. I need to
+# figure out to what extent I can multi-thread this. Definitely not to the
+# extent katago match does since I'll need a separate instance of the victim for
+# each thread. I should check compute/memory usage to do some estimating here
+#   - for leela: maybe 2 threads per GPU?
+# ^^ socat might be better for this, but GPU assignment becomes an issue again.
+# I need to experiment with that before swapping the current twogtp impl for
+# socat
+#   - dumb af solution: what if I just launch socat for every GPU on diff ports
+#   lol
+# there's an annoying aspect to this where multiple threads may try to write to
+# leelaz_opencl_tuning at once? maybe only one thread should have write-access,
+# though i'm not sure i can enforce that tbh. that's actually easier to handle
+# in the docker-in-docker solution --- all but one thread can get a read-only
+# copy
+# also need to make sure each thread gets a separate sgf prefix 
+#
+# TODO should put logs of the two detached containers somewhere? else it's hard
+# to debug if a command fails
+# TODO uhhh this fails really ungracefully if you kill the command midway thru
+# --- your docker containers are still running... maybe docker compose really is
+# more reasonable here
 
 NUM_GAMES=2
 # output directory
@@ -43,16 +57,16 @@ touch -a ${HOST_LEELA_TUNING_FILE}
 # We start the GTP instances first and have gogui-twogtp attach to them. If
 # gogui-twogtp started the GTP instances, then gogui-twogtp would start feeding
 # commands to them before they were ready.
-BLACK_ID=$(docker run --detach --interactive --gpus \"device=0\" --volume ${HOST_LEELA_TUNING_FILE}:/root/.local/share/leela-zero/leelaz_opencl_tuning humancompatibleai/goattack:leela ./run-gtp.sh --debug)
-WHITE_ID=$(docker run --detach --interactive --gpus \"device=0\" humancompatibleai/goattack:elf ./run-gtp.sh --debug)
-# WHITE_ID=$(docker run --detach --interactive --gpus \"device=0\" --volume /home/${USER}/go_attack:/go_attack humancompatibleai/goattack:cpp /engines/KataGo-custom/cpp/katago gtp -config /go_attack/configs/gtp.cfg  -model /dev/null -victim-model /dev/null)
+# BLACK_ID=$(docker run --detach --interactive --gpus \"device=0\" --volume ${HOST_LEELA_TUNING_FILE}:/root/.local/share/leela-zero/leelaz_opencl_tuning humancompatibleai/goattack:leela ./run-gtp.sh)
+BLACK_ID=$(docker run --detach --interactive --gpus \"device=0\" humancompatibleai/goattack:elf ./run-gtp.sh)
+WHITE_ID=$(docker run --detach --interactive --gpus \"device=0\" --volume /nas/ucb/ttseng/go_attack/models:/models --volume ${HOST_REPO_ROOT}/configs:/go_attack/configs humancompatibleai/goattack:cpp /engines/KataGo-custom/cpp/katago gtp -config /go_attack/configs/gtp.cfg  -model /models/adv/t0-s34090496-d8262123.bin.gz -victim-model /models/victim/cp505.bin.gz)
 
 
 docker run --rm --volume ${HOST_DOCKER_SOCKET}:/var/run/docker.sock \
   --volume ${HOST_OUTPUT_DIR}:/output \
   humancompatibleai/goattack:twogtp bin/gogui-twogtp \
   -black "docker attach ${BLACK_ID}" -white "docker attach ${WHITE_ID}" \
-  -alternate -auto -games ${NUM_GAMES} -komi 6.5 -sgffile /output/game \
-  -verbose
+  -alternate -auto -games ${NUM_GAMES} -komi 6.5 -maxmoves 1600 \ 
+  -sgffile /output/game -verbose
 
 docker rm --force ${BLACK_ID} ${WHITE_ID}
