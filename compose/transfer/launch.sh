7e767090ee7d84a1fef7bff73d36b6e91a2bb472
Tom Tseng <tom.hm.tseng@gmail.com>
2022-10-05 12:24:43 -0700

    baseline-transfer: launch.sh: Add support for launching multiple threads

--- compose/transfer/launch.sh
+++ compose/transfer/launch.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-# Directory of this script
-SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )";)
-ENV_FILE=${SCRIPT_DIR}/baseline-attack.env
-export VICTIM_GPU=0
+####################
+# Argument parsing #
+####################
+
+GPUS_STR="all"
+NUM_GAMES_TOTAL=100
+NUM_THREADS=16
 
 function usage() {
-  echo "Usage: $0 [-e ENV_FILE] [-g GPU] [-l LABEL] EXPERIMENT COMMAND"
+  echo "Usage: $0 [-g GPUS] [-l LABEL] [-n NUM_GAMES] [-t NUM_THREADS] EXPERIMENT COMMAND"
   echo
   echo "Launches a transfer experiment."
   echo
   echo "optional arguments:"
-  echo "  -e ENV_FILE, --env-file ENV_FILE"
-  echo "              Which file to use for environment variables."
-  echo "              default: ${ENV_FILE}"
-  echo "  -g GPU, --victim-gpu GPU"
-  echo "              Which GPU to run the victim on."
-  echo "              default: ${VICTIM_GPU}"
+  echo "  -g GPUS, --gpus GPUS"
+  echo "              Which GPUs to run the victim on. This can be a"
+  echo "              comma-separated list (with no spaces in between) or"
+  echo "              'all'."
+  echo "              default: ${VICTIM_GPUS}"
   echo "  -l LABEL, --label LABEL"
   echo "              Label attached to the output directory and docker-compose"
   echo "              project-name. If you are running multiple instances of"
   echo "              the same experiment concurrently, you should specify"
   echo "              unique labels for each instance so the docker-compose"
   echo "              instances don't interfere with each other."
+  echo "  -n NUM_GAMES, --num-games NUM_GAMES"
+  echo "              The total number of games to be played."
+  echo "  -t NUM_THREADS, --num-threads NUM_THREADS"
+  echo "              The number of games to be played at once."
   echo
   echo "positional arguments:"
   echo "  EXPERIMENT  Which experiment to run."
@@ -38,9 +44,10 @@ NUM_POSITIONAL_ARGUMENTS=2
 # Command line flag parsing (https://stackoverflow.com/a/33826763/4865149)
 while [[ "$#" -gt ${NUM_POSITIONAL_ARGUMENTS} ]]; do
   case $1 in
-    -e|--env-file) ENV_FILE=$2; shift ;;
-    -g|--victim-gpu) VICTIM_GPU=$2; shift ;;
+    -g|--gpus) GPUS_STR=$2; shift ;;
     -l|--label) LABEL=$2; shift ;;
+    -n|--num-games) NUM_GAMES_TOTAL=$2; shift ;;
+    -t|--num-threads) NUM_THREADS=$2; shift ;;
     -h|--help) usage; exit 0 ;;
     *) echo "Unknown parameter passed: $1"; usage; exit 1 ;;
   esac
@@ -54,21 +61,59 @@ fi
 EXPERIMENT_NAME=$1
 DOCKER_COMPOSE_COMMAND=$2
 
+GPUS=()
+if [[ "${GPUS_STR}" == "all" ]]; then
+  NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
+  echo "Number of GPUs detected: ${NUM_GPUS}"
+  for (( i = 0; i < NUM_GPUS; i++)) ; do
+    GPUS+=($i)
+  done
+else
+  for i in ${GPUS_STR//,/ }; do  # parse comma-separated string
+    GPUS+=($i)
+  done
+fi
+
+############################
+# Launching the experiment #
+############################
+
+
 # Exported variables like these are used inside the docker-compose yml file.
 export HOST_REPO_ROOT=$(git rev-parse --show-toplevel)
-export HOST_OUTPUT_DIR=${HOST_REPO_ROOT}/transfer-logs/${EXPERIMENT_NAME}/
-[[ -n "${LABEL}" ]] && HOST_OUTPUT_DIR+="${LABEL}-"
-HOST_OUTPUT_DIR+=$(date +%Y%m%d-%H%M%S)
-mkdir --parents ${HOST_OUTPUT_DIR}
+HOST_BASE_OUTPUT_DIR=${HOST_REPO_ROOT}/transfer-logs/${EXPERIMENT_NAME}/
+[[ -n "${LABEL}" ]] && HOST_BASE_OUTPUT_DIR+="${LABEL}-"
+HOST_BASE_OUTPUT_DIR+=$(date +%Y%m%d-%H%M%S)
+mkdir --parents ${HOST_BASE_OUTPUT_DIR}
 
-if [ ${EXPERIMENT_NAME} = "baseline-attack-vs-leela" ]; then
+if [[ "${EXPERIMENT_NAME}" = "baseline-attack-vs-leela" ]]; then
   export HOST_LEELA_TUNING_FILE=${HOST_REPO_ROOT}/engines/leela/leelaz_opencl_tuning
   # Make sure $HOST_LEELA_TUNING_FILE exists.
   touch -a ${HOST_LEELA_TUNING_FILE}
 fi
 
-docker-compose \
-  --file ${SCRIPT_DIR}/${EXPERIMENT_NAME}.yml \
-  --env-file ${ENV_FILE} \
-  --project-name ${EXPERIMENT_NAME}-${LABEL} \
-  ${DOCKER_COMPOSE_COMMAND}
+# Directory of this script
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )";)
+
+if [[ "${DOCKER_COMPOSE_COMMAND}" != "up" ]]; then
+  docker-compose --file ${SCRIPT_DIR}/${EXPERIMENT_NAME}.yml \
+    ${DOCKER_COMPOSE_COMMAND}
+  exit 0
+fi
+
+for (( thread_idx = 0; thread_idx < NUM_THREADS; thread_idx++)) ; do
+  export HOST_OUTPUT_DIR=${HOST_BASE_OUTPUT_DIR}/thread${thread_idx}
+  mkdir --parents ${HOST_OUTPUT_DIR}
+  export NUM_GAMES=$(($NUM_GAMES_TOTAL / $NUM_THREADS + ($thread_idx < ($NUM_GAMES_TOTAL % $NUM_THREADS))))
+  GPU_LEN=${#GPUS[@]}
+  export GPU=${GPUS[(($thread_idx % $GPU_LEN))]}
+  # We don't want multiple threads racing to write to shared files.
+  export ARE_SHARED_FILES_READ_ONLY=$([[ $thread_idx -eq "0" ]] && echo "false" || echo "true")
+
+  docker-compose ${DOCKER_FLAGS} \
+    --file ${SCRIPT_DIR}/${EXPERIMENT_NAME}.yml \
+    --project-name ${EXPERIMENT_NAME}-${LABEL}-thread${thread_idx} \
+    ${DOCKER_COMPOSE_COMMAND} --abort-on-container-exit &
+done
+
+wait $(jobs -p)
