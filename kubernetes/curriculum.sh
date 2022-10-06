0eedb876e49b0005a89954c8208b04adb294317f
Nora Belrose <belrose.nora@gmail.com>
2022-10-06 14:11:42 -0700

    cp127 Elo matching

--- kubernetes/curriculum.sh
+++ kubernetes/curriculum.sh
@@ -5,4 +5,4 @@ python /engines/KataGo-custom/python/curriculum.py \
     -selfplay-dir=/"$VOLUME_NAME"/victimplay/"$RUN_NAME"/selfplay/ \
     -input-models-dir=/"$VOLUME_NAME"/victims \
     -output-models-dir=/"$VOLUME_NAME"/victimplay/"$RUN_NAME"/victims \
-    -config-json-file=/go_attack/configs/curriculum.json
+    -config-json-file=/go_attack/configs/curriculum-visits.json
