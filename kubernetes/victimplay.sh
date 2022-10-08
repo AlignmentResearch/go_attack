bea193864730f80f0c2180606e34d78b513ff23f
Nora Belrose <39116809+norabelrose@users.noreply.github.com>
2022-10-07 19:22:37 -0700

    Shell scripts for training on Kubernetes (#29)
    * Predictor networks for EMCTS
    
    * Shell scripts for training on Kubernetes
    
    * Changed naming scheme for K8 jobs
    
    * Trying to get K8 to work properly
    
    * Script for automatically updating Docker images
    
    * Resetting configs to defaults
    
    * Corrected comment in update_images.py
    
    * Bugfixes
    
    * Shuffle and export doesn't need a GPU
    
    * Stop being stupid about Docker image tags
    
    * Support predictor networks in K8
    
    * Support K8 on Loki
    
    * Adam's suggestions
    
    * Add go_attack files to Python Docker container
    
    * Add go_attack files to Python Dockerfile
    
    * More changes for K8 Loki support
    
    * Incorporate suggestions from Adam and Tom
    
    * Support both Loki and Lambda
    
    * Fix K8 launch scripts
    
    * Don't do pass hardening by default
    
    * Fix stupid mistakes in victimplay.sh
    
    * Add defensive 'mkdir -p' everywhere
    
    * silly commit, revert later
    
    * stupid, revert
    
    * Stupid permissions fix
    
    * Remove -p from mkdir
    
    * Slowly fixing stupid K8 problems
    
    * Fix predictor script
    
    * Remove eval loop from launch-job-predictor.sh
    
    * Add -p flag to the mkdirs again
    
    * Update scripts for breaking change to ctl
    
    * Final fix to K8 script
    
    * Disable vtimeloss & extend curriculum.json
    
    * temporarily changing curriculum
    
    * debugging
    
    * Bug fixes
    
    * playouts -> visits in baseline_attack; improve Game.to_sgf
    
    * Prevent launching K8 job w/ dirty Git repo
    
    * Point KataGo-custom to predictor-nets branch
    
    * Updated KataGo-custom
    
    * Stop passing -nn-predictor-path arg when we don't want to use a predictor
    
    * Removed references to predictor networks
    
    * Various suggestions from Adam
    
    * Cleaned up launch-training.py; other suggestions from Adam
    
    * Add docstrings to launch-training.py
    
    * No darglint, a docstring for zero-argument main() is silly
    
    * Change type annotation for 'service' argument on build_docker_compose_cmd

---
+++ kubernetes/victimplay.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+RUN_NAME="$1"
+VOLUME_NAME="$2"
+mkdir -p /"$VOLUME_NAME"/victimplay/"$RUN_NAME"
+/engines/KataGo-custom/cpp/katago victimplay \
+    -output-dir /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/selfplay/ \
+    -models-dir /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/models/ \
+    -nn-victim-path /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/victims/ \
+    -config /go_attack/configs/active-experiment.cfg \
+    -config /go_attack/configs/compute/1gpu.cfg
