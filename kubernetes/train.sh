67421ce95fd21f014bbc9d2270f4c50ce4aab9af
Tom Tseng <tom.hm.tseng@gmail.com>
2022-10-20 10:51:35 -0700

    warmstart cp63 vs cp505h with lr-scale=0.05

--- kubernetes/train.sh
+++ kubernetes/train.sh
@@ -2,14 +2,40 @@
 cd /engines/KataGo-custom/python
 RUN_NAME="$1"
 VOLUME_NAME="$2"
-INITIAL_WEIGHTS="$3"
+EXPERIMENT_DIR=/"$VOLUME_NAME"/victimplay/"$RUN_NAME"
+
+INITIAL_WEIGHTS="b6c96-s175395328-d26788732" # cp63
+INITIAL_MODEL="kata1-b6c96-s175395328-d26788732.txt.gz"
+
+# INITIAL_WEIGHTS="kata1-b40c256-s11840935168-d2898845681" # cp505
+# INITIAL_MODEL="kata1-b40c256-s11840935168-d2898845681.bin.gz"
+
 if [ -z "$INITIAL_WEIGHTS" ]; then
     echo "No initial weights specified, using random weights"
 else
     echo "Using initial weights: $INITIAL_WEIGHTS"
-    mkdir -p /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/train/t0
-    cp -r /"$VOLUME_NAME"/victim-weights/"$INITIAL_WEIGHTS"/saved_model/model.config.json /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/train/t0/model.config.json
-    cp -r /"$VOLUME_NAME"/victim-weights/"$INITIAL_WEIGHTS"/saved_model/variables /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/train/t0/initial_weights
+
+    INITIAL_WEIGHTS_DIR=/"$VOLUME_NAME"/victim-weights/"$INITIAL_WEIGHTS"/
+    if [ ! -d $INITIAL_WEIGHTS_DIR ]; then
+      echo "Initial weights do not exist: $INITIAL_WEIGHTS_DIR"
+      exit 1
+    fi
+    mkdir -p "$EXPERIMENT_DIR"/train/t0
+    cp "$INITIAL_WEIGHTS_DIR"/saved_model/model.config.json "$EXPERIMENT_DIR"/train/t0/model.config.json
+    cp -r "$INITIAL_WEIGHTS_DIR"/saved_model/variables "$EXPERIMENT_DIR"/train/t0/initial_weights
 fi
 
-./selfplay/train.sh    /"$VOLUME_NAME"/victimplay/"$RUN_NAME"    t0    b6c96    256    main    -disable-vtimeloss    -lr-scale 1.0    -max-train-bucket-per-new-data 4
+if [ -n "$INITIAL_MODEL" ] && [ ! -f "$EXPERIMENT_DIR"/done-copying-initial-model ]; then
+    echo "Using initial model: $INITIAL_MODEL"
+    INITIAL_MODEL=/"$VOLUME_NAME"/victims/$INITIAL_MODEL
+    if [ ! -f $INITIAL_MODEL ]; then
+      echo "Initial model does not exist: $INITIAL_MODEL"
+      exit 1
+    fi
+    MODEL_EXTENSION=${INITIAL_MODEL: -6} # bin.gz or txt.gz
+    mkdir -p "$EXPERIMENT_DIR"/models/t0-s0-d0
+    cp "$INITIAL_MODEL" "$EXPERIMENT_DIR"/models/t0-s0-d0/model."$MODEL_EXTENSION"
+    touch "$EXPERIMENT_DIR"/done-copying-initial-model
+fi
+
+./selfplay/train.sh    /"$VOLUME_NAME"/victimplay/"$RUN_NAME"    t0    b6c96    256    main    -disable-vtimeloss    -lr-scale 0.05   -max-train-bucket-per-new-data 4
--- kubernetes/victimplay.sh
+++ kubernetes/victimplay.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 RUN_NAME="$1"
 VOLUME_NAME="$2"
+
+while [ ! -f /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/done-copying-initial-model ]; do
+  # dumb hack. maybe this script should do the copying instead of train.sh, i
+  # just don't want to think about how to deal with this when there are multiple
+  # replicas running this script
+  echo "Waiting for train.sh to copy initial model"
+  sleep 30;
+done
+
 mkdir -p /"$VOLUME_NAME"/victimplay/"$RUN_NAME"
 /engines/KataGo-custom/cpp/katago victimplay \
     -output-dir /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/selfplay/ \
