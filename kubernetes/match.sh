612c9589f6016b518006e39c9770c7970dabea4a
Tom Tseng <tom.hm.tseng@gmail.com>
2022-11-01 22:24:21 -0700

    hash

--- kubernetes/match.sh
+++ kubernetes/match.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Usage: $0 OUTPUT_DIR NUM_GAMES
+#
+# If NUM_GAMES is -1, then the number of games from `match-base.cfg` will be
+# used.
 
 OUTPUT_DIR=$1
 NUM_GAMES=$2
@@ -6,11 +10,13 @@ shift; shift
 if [ "${NUM_GAMES}" -ge 0 ]; then
   GAMES_OVERRIDE="-override-config numGamesTotal=${NUM_GAMES}"
 fi
+# Each replica of `match` should output to a different log file.
+ID=$(openssl rand -hex 4)
 mkdir --parents "${OUTPUT_DIR}"
 # shellcheck disable=SC2068,SC2086
 /engines/KataGo-custom/cpp/katago match \
   -config /go_attack/configs/match-base.cfg \
   -config /go_attack/configs/compute/1gpu.cfg \
   -sgf-output-dir "${OUTPUT_DIR}"/sgfs \
-  -log-file "${OUTPUT_DIR}"/match.log \
+  -log-file "${OUTPUT_DIR}"/match-"${ID}".log \
   $GAMES_OVERRIDE $@
