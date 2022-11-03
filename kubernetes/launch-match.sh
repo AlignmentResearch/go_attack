3ecf5ae1d981f5bc6c67f6d17ca8f6dfdfff7ebd
Nora Belrose <belrose.nora@gmail.com>
2022-11-03 16:08:55 -0700

    Require victim and adversary to be specified

--- kubernetes/launch-match.sh
+++ kubernetes/launch-match.sh
@@ -9,12 +9,14 @@ DEFAULT_NUM_GPUS=1
 usage() {
   echo "Schedules a job that runs \`match\`."
   echo
-  echo "Usage: $0 [--gpus GPUS] [--games NUM_GAMES] [--use-weka] PREFIX"
+  echo "Usage: $0 [--gpus GPUS] [--games NUM_GAMES] [--use-weka] PREFIX VICTIM ADVERSARY"
   echo "          [--EXTRA_MATCH_FLAGS]"
   echo
   echo "positional arguments:"
-  echo "  PREFIX  Identifying label used for the name of the job and the name"
-  echo "          of the output directory."
+  echo "  PREFIX     Identifying label used for the name of the job and the name"
+  echo "             of the output directory."
+  echo "  VICTIM     Filename of the victim checkpoint inside the container."
+  echo "  ADVERSARY  Filename of the adversary checkpoint inside the container."
   echo
   echo "optional arguments:"
   echo "  -g GPUS, --gpus GPUS"
@@ -35,7 +37,7 @@ usage() {
   echo "  $0 test-run -- -override-config nnModelFile0=/dev/null"
 }
 
-NUM_POSITIONAL_ARGUMENTS=1
+NUM_POSITIONAL_ARGUMENTS=3
 
 NUM_GPUS=${DEFAULT_NUM_GPUS}
 # Command line flag parsing (https://stackoverflow.com/a/33826763/4865149)
@@ -56,9 +58,11 @@ if [ $# -lt ${NUM_POSITIONAL_ARGUMENTS} ]; then
 fi
 
 PREFIX=$1
+VICTIM=$2
+ADVERSARY=$3
 RUN_NAME="$PREFIX-$(date +%Y%m%d-%H%M%S)"
 echo "Run name: $RUN_NAME"
-shift
+shift 3
 
 if [ $# -gt 0 ]; then
   if [ "$1" != "--" ]; then
@@ -92,7 +96,13 @@ fi
 ctl job run --container \
   "$CPP_IMAGE" \
   $VOLUME_FLAGS \
-  --command "bash -x /go_attack/kubernetes/match.sh /shared/match/${RUN_NAME} ${GAMES_PER_REPLICA} $*" \
+  --command "bash -x
+  /go_attack/kubernetes/match.sh
+  /shared/match/${RUN_NAME}
+  ${GAMES_PER_REPLICA}
+  ${VICTIM}
+  ${ADVERSARY}
+  $*" \
   --gpu 1 \
   --name go-match-"$PREFIX" \
   --replicas "${NUM_GPUS}"
