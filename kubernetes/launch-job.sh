ee5f3231c6b21b7b8bfb22d382e93d3ea1722d20
Adam Gleave <adam@gleave.me>
2022-11-03 22:50:58 -0700

    Add back missing source

--- kubernetes/launch-job.sh
+++ kubernetes/launch-job.sh
@@ -65,6 +65,7 @@ RUN_NAME="$1-${RESUME_TIMESTAMP:-$(date +%Y%m%d-%H%M%S)}"
 echo "Run name: $RUN_NAME"
 
 VOLUME_NAME="shared"
+source "$(dirname "$(readlink -f "$0")")"/launch-common.sh
 update_images "cpp python"
 
 # shellcheck disable=SC2215,SC2086,SC2089,SC2090
