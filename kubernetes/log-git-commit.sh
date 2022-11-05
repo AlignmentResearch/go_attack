a972f8ba126ccfeaa83ade004a341ddfe861ec51
Tom Tseng <tom.hm.tseng@gmail.com>
2022-11-04 21:23:47 -0700

    log commit

---
+++ kubernetes/log-git-commit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+# Logs the git commit (assumed to be saved in the GIT_COMMIT environment
+# variable) used for an experiment.
+
+OUTPUT_DIR=$1
+
+mkdir -p "$OUTPUT_DIR"
+COMMIT_FILE="$OUTPUT_DIR"/commit
+GIT_COMMIT=${GIT_COMMIT:-no_commit_found}
+
+if [ -f "$COMMIT_FILE" ]; then
+  LATEST_COMMIT=$(tail -n 1 "$COMMIT_FILE" | awk '{print $2}')
+  if [ "$LATEST_COMMIT" != "$GIT_COMMIT" ]; then
+    echo "$(date +%Y%m%d-%H%M%S) $GIT_COMMIT" >> "$COMMIT_FILE"
+  fi
+else
+  echo "$(date +%Y%m%d-%H%M%S) $GIT_COMMIT" >> "$COMMIT_FILE"
+fi
