f850c2dd68def38c8140127d77109a935b64bb98
Tom Tseng <tom.hm.tseng@gmail.com>
2022-11-07 09:26:46 -0800

    adam comments

--- kubernetes/curriculum.sh
+++ kubernetes/curriculum.sh
@@ -2,9 +2,6 @@
 RUN_NAME="$1"
 VOLUME_NAME="$2"
 
-# not related to curriculum but we want some process to log this
-/go_attack/kubernetes/log-git-commit.sh /"$VOLUME_NAME"/victimplay/"$RUN_NAME"
-
 python /engines/KataGo-custom/python/curriculum.py \
     -selfplay-dir=/"$VOLUME_NAME"/victimplay/"$RUN_NAME"/selfplay/ \
     -input-models-dir=/"$VOLUME_NAME"/victims \
--- kubernetes/log-git-commit.sh
+++ kubernetes/log-git-commit.sh
@@ -9,12 +9,10 @@ COMMIT_FILE="$OUTPUT_DIR"/commit
 GIT_COMMIT=${GIT_COMMIT:-no_commit_found}
 
 if [ -f "$COMMIT_FILE" ]; then
-  # COMMIT_FILE already exists, but we may still want to log the commit if the
-  # commit has changed due to restarting a job.
   LATEST_COMMIT=$(tail -n 1 "$COMMIT_FILE" | awk '{print $2}')
-  if [ "$LATEST_COMMIT" != "$GIT_COMMIT" ]; then
-    echo "$(date +%Y%m%d-%H%M%S) $GIT_COMMIT" >> "$COMMIT_FILE"
+  if [ "$LATEST_COMMIT" = "$GIT_COMMIT" ]; then
+    exit 0
   fi
-else
-  echo "$(date +%Y%m%d-%H%M%S) $GIT_COMMIT" >> "$COMMIT_FILE"
 fi
+
+echo "$(date +%Y%m%d-%H%M%S) $GIT_COMMIT" >> "$COMMIT_FILE"
