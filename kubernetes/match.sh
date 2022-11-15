0f5ce1a5ae3826d99b713cbb27e6bd14786a20a2
Tom Tseng <tom.hm.tseng@gmail.com>
2022-11-14 20:35:24 -0800

    match: s497721856-v200-ov1 sweep vic visit count

--- kubernetes/match.sh
+++ kubernetes/match.sh
@@ -19,7 +19,7 @@ ID=$(openssl rand -hex 4)
 mkdir --parents "${OUTPUT_DIR}"
 # shellcheck disable=SC2068,SC2086
 /engines/KataGo-custom/cpp/katago match \
-  -config /go_attack/configs/match-base.cfg \
+  -config /go_attack/configs/match.cfg \
   -config /go_attack/configs/compute/1gpu.cfg \
   -sgf-output-dir "${OUTPUT_DIR}"/sgfs \
   -log-file "${OUTPUT_DIR}"/match-"${ID}".log \
