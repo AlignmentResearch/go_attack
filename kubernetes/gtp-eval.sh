8b840f9a6794b0786361a7da3cd63143f29f8a4a
Tom Tseng <tom.hm.tseng@gmail.com>
2023-01-13 02:39:50 -0800

    prep for real run

--- kubernetes/gtp-eval.sh
+++ kubernetes/gtp-eval.sh
@@ -8,6 +8,5 @@ mkdir --parents "$RUN_NAME/sgfs" && \
   -black '/engines/KataGo-raw/cpp/katago gtp -config /go_attack/configs/gtp-raw.cfg -model /shared/victims/kata1-b40c256-s11840935168-d2898845681.bin.gz' \
   -white '/engines/KataGo-custom/cpp/katago gtp -config /go_attack/configs/gtp-amcts.cfg -model /shared/victimplay/ttseng-avoid-pass-alive-coldstart-39-20221025-175949/models/t0-s545065216-d136760487/model.bin.gz -victim-model /shared/victims/kata1-b40c256-s11840935168-d2898845681.bin.gz' \
   -alternate -auto -games "$NUM_GAMES" -komi 6.5 -maxmoves 1600 \
-  -sgffile /outputs/sgfs/game -verbose \
+  -sgffile "$RUN_NAME/sgfs/game" -verbose \
 2>&1 | tee --append "$RUN_NAME/twogtp.log"
-
--- kubernetes/launch-gtp-eval.sh
+++ kubernetes/launch-gtp-eval.sh
@@ -2,7 +2,7 @@
 
 source "$(dirname "$(readlink -f "$0")")"/launch-common.sh
 
-cd ~/go_attack
+cd ~/go_attack || exit
 docker build . -f compose/cpp/Dockerfile -t humancompatibleai/goattack:cpp
 GIT_COMMIT=$(git rev-parse --short HEAD)
 TWOGTP_IMG="humancompatibleai/goattack:twogtp-and-cpp-$GIT_COMMIT"
@@ -10,18 +10,20 @@ docker build . -f compose/transfer/twogtp-and-cpp/Dockerfile -t "$TWOGTP_IMG"
 docker push "$TWOGTP_IMG"
 echo "$TWOGTP_IMG"
 
-# NUM_GPUS=7
-# PREFIX=ttseng-v10mil
-# RUN_NAME="$PREFIX-$(date +%Y%m%d-%H%M%S)"
+NUM_GPUS=7
+NUM_CPUS=224
+PREFIX=ttseng-v10mil
+RUN_NAME="$PREFIX-$(date +%Y%m%d-%H%M%S)"
 
-# ~/sleipnir/ctl/ctl/ctl.py job run --container \
-#   "$TWOGTP_IMG" \
-#   $VOLUME_FLAGS \
-#   --command "bash -x
-#   /go_attack/kubernetes/gtp-eval.sh
-#   /shared/eval/$RUN_NAME" \
-#   --high-priority \
-#   --shared-host-dir-slow-tolerant \
-#   --gpu "$NUM_GPUS" \
-#   --name "go-gtp-$PREFIX" \
-#   --replicas 1
+~/sleipnir/ctl/ctl/ctl.py job run --container \
+  "$TWOGTP_IMG" \
+  $VOLUME_FLAGS \
+  --command "bash -x
+  /go_attack/kubernetes/gtp-eval.sh
+  /shared/eval/$RUN_NAME" \
+  --high-priority \
+  --shared-host-dir-slow-tolerant \
+  --gpu "$NUM_GPUS" \
+  --cpu "$NUM_CPUS" \
+  --name "go-gtp-$PREFIX" \
+  --replicas 1
