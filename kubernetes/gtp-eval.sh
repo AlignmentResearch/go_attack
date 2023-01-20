acd4177a8a53d8450f78318ed41ac5071f6995fd
Tom Tseng <tom.hm.tseng@gmail.com>
2023-01-20 13:50:54 -0800

    parse params

--- kubernetes/gtp-eval.sh
+++ kubernetes/gtp-eval.sh
@@ -1,17 +1,28 @@
 #!/bin/bash -eu
+# Arguments:
+# - Output directory
+# - Number of GPUs
+# - Number of games
+# - Adversary config
+# - Victim config
+# - Adversary model
+# - Victim model
 
 OUTPUT_DIR=$1
 NUM_GPUS=$2
-NUM_GAMES=100
+NUM_GAMES=$3
 GPU_CONFIG="go_attack/configs/compute/${NUM_GPUS}gpu.cfg"
-VICTIM_MODEL=/shared/victims/kata1-b40c256-s11840935168-d2898845681.bin.gz
+ADV_CONFIG=$4
+VICTIM_CONFIG=$5
+ADV_MODEL=$6
+VICTIM_MODEL=$7
 
 /go_attack/kubernetes/log-git-commit.sh "$OUTPUT_DIR"
 
 mkdir --parents "$OUTPUT_DIR/sgfs" && \
 /gogui/bin/gogui-twogtp \
-  -black "/engines/KataGo-raw/cpp/katago gtp -config /go_attack/configs/gtp-raw.cfg -config $GPU_CONFIG -model $VICTIM_MODEL" \
-  -white "/engines/KataGo-custom/cpp/katago gtp -config /go_attack/configs/gtp-amcts.cfg -config $GPU_CONFIG -model /shared/victimplay/ttseng-avoid-pass-alive-coldstart-39-20221025-175949/models/t0-s545065216-d136760487/model.bin.gz -victim-model $VICTIM_MODEL" \
+  -black "/engines/KataGo-raw/cpp/katago gtp -config $VICTIM_CONFIG -config $GPU_CONFIG -model $VICTIM_MODEL" \
+  -white "/engines/KataGo-custom/cpp/katago gtp -config $ADV_CONFIG -config $GPU_CONFIG -model $ADV_MODEL -victim-model $VICTIM_MODEL" \
   -alternate -auto -games "$NUM_GAMES" -komi 6.5 -maxmoves 1600 \
   -sgffile "$OUTPUT_DIR/sgfs/game" -verbose \
 2>&1 | tee --append "$OUTPUT_DIR/twogtp.log"
--- kubernetes/launch-gtp-eval.sh
+++ kubernetes/launch-gtp-eval.sh
@@ -1,14 +1,22 @@
 #!/bin/bash -eu
 
+HOST_REPO_ROOT=$(git rev-parse --show-toplevel)
+
 DEFAULT_NUM_CPUS=8
 DEFAULT_NUM_GAMES=100
 DEFAULT_NUM_GPUS=1
+DEFAULT_ADV_CONFIG=/go_attack/configs/gtp-amcts.cfg
+DEFAULT_VICTIM_CONFIG=/go_attack/configs/gtp-raw.cfg
+DEFAULT_ADV_MODEL=/shared/victimplay/ttseng-avoid-pass-alive-coldstart-39-20221025-175949/models/t0-s545065216-d136760487/model.bin.gz
+DEFAULT_VICTIM_MODEL=/shared/victims/kata1-b40c256-s11840935168-d2898845681.bin.gz
 
 usage() {
   echo "Schedules a job that plays a victim (on KataGo-raw) against an A-MCTS"
   echo "adversary via GTP."
   echo
-  echo "Usage: $0 [--cpus CPUS] [--gpus GPUS] [--games NUM_GAMES] [--use-weka] PREFIX"
+  echo "Usage: $0 [--cpus CPUS] [--gpus GPUS] [--games NUM_GAMES] [--use-weka]"
+  echo "         [--adv-config ADV_CONFIG] [--victim-config VICTIM_CONFIG]"
+  echo "         [--adv-config ADV_MODEL] [--victim-config VICTIM_MODEL] PREFIX"
   echo
   echo "positional arguments:"
   echo "  PREFIX     Identifying label used for the name of the job and the name"
@@ -27,12 +35,29 @@ usage() {
   echo "    Store results on the go-attack Weka volume instead of the CHAI NAS"
   echo "    volume."
   echo
+  echo "  --adv-config ADV_CONFIG"
+  echo "              Config for adversary to use."
+  echo "              default: ${DEFAULT_ADV_CONFIG}"
+  echo "  --victim-config VICTIM_CONFIG"
+  echo "              Config for victim to use."
+  echo "              default: ${DEFAULT_VICTIM_CONFIG}"
+  echo "  --adv-model ADV_MODEL"
+  echo "              Model for adversary to use."
+  echo "              default: ${DEFAULT_ADV_MODEL}"
+  echo "  --victim-model VICTIM_MODEL"
+  echo "              Model for victim to use."
+  echo "              default: ${DEFAULT_VICTIM_MODEL}"
+  echo
   echo "Optional arguments should be specified before positional arguments."
 }
 
 NUM_CPUS=${DEFAULT_NUM_CPUS}
 NUM_GAMES=${DEFAULT_NUM_GAMES}
 NUM_GPUS=${DEFAULT_NUM_GPUS}
+ADV_CONFIG=${DEFAULT_ADV_CONFIG}
+VICTIM_CONFIG=${DEFAULT_VICTIM_CONFIG}
+ADV_MODEL=${DEFAULT_ADV_MODEL}
+VICTIM_MODEL=${DEFAULT_VICTIM_MODEL}
 while [ -n "${1-}" ]; do
   case $1 in
     -h|--help) usage; exit 0 ;;
@@ -40,6 +65,10 @@ while [ -n "${1-}" ]; do
     -g|--gpus) NUM_GPUS=$2; shift ;;
     -n|--games) NUM_GAMES=$2; shift ;;
     -w|--use-weka) export USE_WEKA=1 ;;
+    --adv-config) ADV_CONFIG=$2; shift ;;
+    --victim-config) VICTIM_CONFIG=$2; shift ;;
+    --adv-model) ADV_MODEL=$2; shift ;;
+    --victim-model) VICTIM_MODEL=$2; shift ;;
     *) break ;;
   esac
   shift
