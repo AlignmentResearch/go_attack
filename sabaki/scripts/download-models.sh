e3e0bfa48e37fe5182f9ca1f9f3a7fd406534790
Tony Wang <tong.t.wang@gmail.com>
2023-01-25 21:45:53 -0500

    Improve download-models script

--- sabaki/scripts/download-models.sh
+++ sabaki/scripts/download-models.sh
@@ -5,9 +5,18 @@ mkdir -p $GIT_ROOT/models/adv
 mkdir -p $GIT_ROOT/models/victims
 
 # Download models
-wget 'https://drive.google.com/uc?export=download&id=1Qktfjfr6YwplF50T-LMXkFK_qG1UXeiy' \
-     -O $GIT_ROOT/models/adv/adv505h-s349284096-d87808728.bin.gz
-wget 'https://drive.google.com/uc?export=download&id=1TDSwJ_i0CHF_Ksf7lOaQIorpMu976fja' \
-     -O $GIT_ROOT/models/adv/adv505h-s497721856-d125043118.bin.gz
-wget 'https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b40c256-s11840935168-d2898845681.bin.gz' \
-     -O $GIT_ROOT/models/victims/kata1-b40c256-s11840935168-d2898845681.bin.gz
+
+# adv505h-s349284096-d87808728.bin.gz
+wget --content-disposition 'https://drive.google.com/uc?export=download&id=1Qktfjfr6YwplF50T-LMXkFK_qG1UXeiy' \
+     -P $GIT_ROOT/models/adv/ --content-disposition
+
+# adv505h-s497721856-d125043118.bin.gz
+wget --content-disposition 'https://drive.google.com/uc?export=download&id=1TDSwJ_i0CHF_Ksf7lOaQIorpMu976fja' \
+     -P $GIT_ROOT/models/adv/ --content-disposition
+
+# adv505h-s545065216-d136760487.bin.gz
+wget --content-disposition 'https://drive.google.com/uc?export=download&id=1gwD0nQsuE7aD92YJ66l82qtXR97A_lt1' \
+     -P $GIT_ROOT/models/adv/
+
+wget --content-disposition 'https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b40c256-s11840935168-d2898845681.bin.gz' \
+     -P $GIT_ROOT/models/victims/
