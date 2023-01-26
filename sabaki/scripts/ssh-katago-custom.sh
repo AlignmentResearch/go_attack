c53b60e3591d5c0442a83af11813fce5a9c3f304
Tony Wang <tong.t.wang@gmail.com>
2023-01-25 21:25:32 -0500

    Add rnn sabaki script

--- sabaki/scripts/ssh-katago-custom.sh
+++ sabaki/scripts/ssh-katago-custom.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ssh rnn -t 'bash -l -c "/nas/ucb/tony/go-attack/gtp-host/go_attack/sabaki/scripts/docker-katago-custom.sh $@"'
+bash_command="/nas/ucb/tony/go-attack/gtp-host/go_attack/sabaki/scripts/docker-katago-custom.sh $@"
+echo ${bash_command}
+ssh rnn -tt 'bash -l -c "'${bash_command}'"'
