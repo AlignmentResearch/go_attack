6b7cdd1f255b9211cb8fcb1fba9aeb42a36eb07b
Tom Tseng <tom.hm.tseng@gmail.com>
2023-02-01 17:09:20 -0800

    pass harden the adversary, warmstart at cp63

--- kubernetes/victimplay.sh
+++ kubernetes/victimplay.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 RUN_NAME="$1"
 VOLUME_NAME="$2"
+
+while [ ! -f /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/done-copying-initial-model ]; do
+  echo "Waiting for train.sh to copy initial model"
+  sleep 30;
+done
+
 mkdir -p /"$VOLUME_NAME"/victimplay/"$RUN_NAME"
 /engines/KataGo-custom/cpp/katago victimplay \
     -output-dir /"$VOLUME_NAME"/victimplay/"$RUN_NAME"/selfplay/ \
