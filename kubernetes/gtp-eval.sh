bb97ef43c29aee77e5acf69c3ec68eaa85ae7403
Tom Tseng <tom@alignmentfund.org>
2023-02-06 13:59:48 -0800

    gtp-eval nits

--- kubernetes/gtp-eval.sh
+++ kubernetes/gtp-eval.sh
@@ -17,13 +17,14 @@ ADV_MODEL=$6
 VICTIM_MODEL=$7
 
 GPU_CONFIG="go_attack/configs/compute/${NUM_GPUS}gpu.cfg"
+COMMON_KATAGO_FLAGS="-config $GPU_CONFIG -override-config logSearchInfo=true,logToStdout=true"
 
 /go_attack/kubernetes/log-git-commit.sh "$OUTPUT_DIR"
 
-mkdir --parents "$OUTPUT_DIR/sgfs" && \
+mkdir --parents "$OUTPUT_DIR/sgfs"
 /gogui/bin/gogui-twogtp \
-  -black "/engines/KataGo-raw/cpp/katago gtp -config $VICTIM_CONFIG -config $GPU_CONFIG -override-config logSearchInfo=true,logToStdout=true -model $VICTIM_MODEL" \
-  -white "/engines/KataGo-custom/cpp/katago gtp -config $ADV_CONFIG -config $GPU_CONFIG -override-config logSearchInfo=true,logToStdout=true -model $ADV_MODEL -victim-model $VICTIM_MODEL" \
+  -black "/engines/KataGo-raw/cpp/katago gtp -config $VICTIM_CONFIG -config $GPU_CONFIG $COMMON_KATAGO_FLAGS -model $VICTIM_MODEL" \
+  -white "/engines/KataGo-custom/cpp/katago gtp -config $ADV_CONFIG -config $GPU_CONFIG $COMMON_KATAGO_FLAGS -model $ADV_MODEL -victim-model $VICTIM_MODEL" \
   -alternate -auto -games "$NUM_GAMES" -komi 6.5 -maxmoves 1600 \
   -sgffile "$OUTPUT_DIR/sgfs/game" -verbose \
 2>&1 | tee --append "$OUTPUT_DIR/twogtp.log"
