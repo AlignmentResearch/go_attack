e1f645f903e7608fa6b2d889672057231af65dbe
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2023-03-15 11:16:37 -0700

    Plotted activations as videos

--- interp/plot_katago_activations.py
+++ interp/plot_katago_activations.py
@@ -29,9 +29,41 @@ except NameError:
     assert os.path.basename(go_attack_path) == "go_attack", "Don't know how to find the go_attack directory"
 
 sys.path.insert(0, os.path.join(go_attack_path, "engines/KataGo-custom/python"))
+sys.path.append("/usr/lib/python3.8/site-packages")
+sys.path.append("/opt/venv/lib/python3.8/site-packages")
+
 from board import Board
 from model import Model
 import common
+
+# %% Load plotly
+import plotly
+import plotly.express as px
+
+from IPython.display import Image, FileLink
+
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.tools as tls
+import hashlib
+
+
+def myshow(self, *args, **kwargs):
+    html = pio.to_html(self)
+    mhash = hashlib.md5(html.encode("utf-8")).hexdigest()
+    if not os.path.isdir(".ob-jupyter"):
+        os.mkdir(".ob-jupyter")
+    fhtml = os.path.join(".ob-jupyter", mhash + ".html")
+
+    with open(fhtml, "w") as f:
+        f.write(html)
+
+    # display(FileLink(fhtml, result_html_suffix=''))
+    # return Image(pio.to_image(self, 'png'))
+    return FileLink(fhtml, result_html_suffix="")
+
+
+go.Figure.show = myshow
 
 
 # %% Load model from saved files
@@ -89,16 +121,19 @@ RULES = {
 
 
 class GameState:
-    def __init__(self, board_size, rules=None):
+    def __init__(self, board_size, board=None, rules=None):
         self.board_size = board_size
-        self.board = Board(size=board_size)
+        if board is None:
+            board = Board(size=board_size)
+        self.board = board
+        assert self.board_size == self.board.size
+
         self.moves = []
         self.boards = [self.board.copy()]
         self.rules = rules if rules is not None else RULES
 
     def copy(self):
-        gs = GameState(self.board_size, rules=self.rules.copy())
-        gs.board = self.board.copy()
+        gs = GameState(self.board_size, board=self.board.copy(), rules=self.rules.copy())
         gs.moves = self.moves.copy()
         gs.boards = self.boards.copy()
         return gs
@@ -436,7 +471,7 @@ gs = GameState(POS_LEN)
 # %% Run the model and plot the activations
 
 policy = sess.run(model.policy_output, feed_dict=gs.feed_dict())
-gs.show(policy)
+gs.show(policy[0, :, 0])
 
 # %% Check which side is white or black
 
@@ -456,39 +491,312 @@ gs.show(outs["policy1"])
 
 # %% Construct the two slightly different tricky positions
 
-game = "AW[cb][db][eb][fb][gb][hb][ib][jb][kb][lb][mb][nb][ob][pb][qb][bc][rc][bd][cd][ed][fd][gd][id][jd][kd][md][od][pd][rd][be][ee][ge][ie][ke][me][pe][re][bf][df][ef][gf][if][kf][mf][of][pf][rf][bg][eg][gg][ig][kg][mg][pg][rg][sg][bh][ch][eh][gh][ih][kh][mh][oh][ph][rh][sh][bi][ci][ei][gi][ii][ki][mi][pi][bj][ej][gj][ij][kj][mj][oj][pj][bk][ek][gk][ik][kk][mk][pk][bl][cl][el][gl][il][kl][ml][ol][pl][sl][bm][em][gm][im][km][mm][nm][pm][rm][sm][bn][cn][en][gn][in][kn][mn][pn][rn][sn][bo][eo][go][io][ko][mo][po][ro][so][bp][dp][ep][gp][hp][ip][kp][lp][mp][np][op][pp][rp][bq][rq][cr][dr][er][fr][gr][hr][ir][jr][kr][lr][mr][nr][or][pr][qr]AB[cc][dc][ec][fc][gc][hc][ic][jc][kc][lc][mc][nc][oc][pc][qc][dd][hd][ld][nd][qd][ce][de][fe][he][je][le][ne][oe][qe][cf][ff][hf][jf][lf][nf][qf][cg][dg][fg][hg][jg][lg][ng][og][qg][dh][fh][hh][jh][lh][nh][qh][di][fi][hi][ji][li][ni][oi][qi][cj][dj][fj][hj][jj][lj][nj][qj][rj][sj][ck][dk][fk][hk][jk][lk][nk][ok][qk][sk][dl][fl][hl][jl][ll][nl][ql][rl][cm][dm][fm][hm][jm][lm][qm][dn][fn][hn][jn][ln][on][qn][co][do][fo][ho][jo][lo][qo][cp][fp][jp][qp][cq][dq][eq][fq][gq][hq][iq][jq][kq][lq][mq][nq][oq][pq][qq]"
+from pysgf import SGF
 
-white, black = game.split("AB")
-white = white[3:-1].split("][")
-black = black[1:-1].split("][")
 
-white_pos = [(ord(w[0]) - ord("a"), ord(w[1]) - ord("a")) for w in white]
-black_pos = [(ord(b[0]) - ord("a"), ord(b[1]) - ord("a")) for b in black]
+def sgf_to_gs(game):
+    gs = GameState(board_size=int(game.get_list_property("SZ")[0]))
+    for pos_str in game.get_list_property("AB"):
+        pos = (ord(pos_str[0]) - ord("a"), ord(pos_str[1]) - ord("a"))
+        gs.play(Board.BLACK, pos)
+    for pos_str in game.get_list_property("AW"):
+        pos = (ord(pos_str[0]) - ord("a"), ord(pos_str[1]) - ord("a"))
+        gs.play(Board.WHITE, pos)
 
-gs = GameState(POS_LEN)
+    node = game
+    assert node.move is None
 
-len_diff = len(white_pos) - len(black_pos)
-assert len_diff >= 0
+    while len(node.children) > 0:
+        node = node.children[0]
+        assert node.move is not None
+        player = Board.BLACK if node.move.player == "B" else Board.WHITE
+        loc = node.move.coords
+        gs.play(player, (loc[0], 18 - loc[1]))
 
-for i in range(len_diff):
-    gs.play(Board.WHITE, white_pos[i])
+    return gs
 
-for w, b in zip(white_pos[len_diff:], black_pos):
-    if b != (3, 9):
-        gs.play(Board.BLACK, b)
-    gs.play(Board.WHITE, w)
 
-gs_nocycle = gs.copy()
+gs_cycle = sgf_to_gs(SGF.parse_file("connection_test_position_A.sgf"))
+gs_nocycle = sgf_to_gs(SGF.parse_file("connection_test_position_B.sgf"))
 
-# TODO figure out how to do this with proper history
-gs.play(Board.BLACK, (3, 9))
-gs.show()
+
+# %% Check winrate of the two positions
+
+
+def print_scorebelief(gs, outputs):
+    board = gs.board
+    scorebelief = outputs["scorebelief"]
+    scoremean = outputs["scoremean"]
+    scorestdev = outputs["scorestdev"]
+    sbscale = outputs["sbscale"]
+
+    scorebelief = list(scorebelief)
+    if board.pla != Board.WHITE:
+        scorebelief.reverse()
+        scoremean = -scoremean
+
+    scoredistrmid = POS_LEN * POS_LEN + Model.EXTRA_SCORE_DISTR_RADIUS
+    ret = ""
+    ret += "TEXT "
+    ret += "SBScale: " + str(sbscale) + "\n"
+    ret += "ScoreBelief: \n"
+    for i in range(17, -1, -1):
+        ret += "TEXT "
+        ret += "%+6.1f" % (-(i * 20 + 0.5))
+        for j in range(20):
+            idx = scoredistrmid - (i * 20 + j) - 1
+            ret += " %4.0f" % (scorebelief[idx] * 10000)
+        ret += "\n"
+    for i in range(18):
+        ret += "TEXT "
+        ret += "%+6.1f" % ((i * 20 + 0.5))
+        for j in range(20):
+            idx = scoredistrmid + (i * 20 + j)
+            ret += " %4.0f" % (scorebelief[idx] * 10000)
+        ret += "\n"
+
+    beliefscore = 0
+    beliefscoresq = 0
+    beliefwin = 0
+    belieftotal = 0
+    for idx in range(scoredistrmid * 2):
+        score = idx - scoredistrmid + 0.5
+        if score > 0:
+            beliefwin += scorebelief[idx]
+        else:
+            beliefwin -= scorebelief[idx]
+        belieftotal += scorebelief[idx]
+        beliefscore += score * scorebelief[idx]
+        beliefscoresq += score * score * scorebelief[idx]
+
+    beliefscoremean = beliefscore / belieftotal
+    beliefscoremeansq = beliefscoresq / belieftotal
+    beliefscorevar = max(0, beliefscoremeansq - beliefscoremean * beliefscoremean)
+    beliefscorestdev = math.sqrt(beliefscorevar)
+
+    ret += "TEXT BeliefWin: %.2fc\n" % (100 * beliefwin / belieftotal)
+    ret += "TEXT BeliefScoreMean: %.1f\n" % (beliefscoremean)
+    ret += "TEXT BeliefScoreStdev: %.1f\n" % (beliefscorestdev)
+    ret += "TEXT ScoreMean: %.1f\n" % (scoremean)
+    ret += "TEXT ScoreStdev: %.1f\n" % (scorestdev)
+    ret += "TEXT Value: %s\n" % (str(outputs["value"]))
+    ret += "TEXT TDValue: %s\n" % (str(outputs["td_value"]))
+    ret += "TEXT TDValue2: %s\n" % (str(outputs["td_value2"]))
+    ret += "TEXT TDValue3: %s\n" % (str(outputs["td_value3"]))
+    ret += "TEXT TDScore: %s\n" % (str(outputs["td_score"]))
+    ret += "TEXT Estv: %s\n" % (str(outputs["estv"]))
+    ret += "TEXT Ests: %s\n" % (str(outputs["ests"]))
+    return ret
+
+
+print(print_scorebelief(gs_cycle, gs_cycle.get_outputs(sess, model)))
+print(print_scorebelief(gs_nocycle, gs_nocycle.get_outputs(sess, model)))
 
 # %%
 
-gs_nocycle.play(Board.WHITE, (3, 9))
-gs_nocycle.show()
+cycle_ins = gs_cycle.feed_dict()[model.bin_inputs]
+nocycle_ins = gs_nocycle.feed_dict()[model.bin_inputs]
+
+batch, pos, chans = np.where(cycle_ins != nocycle_ins)
+
+# %% [markdown]
+"""
+The differences are:
+ - in positions (12, 7), (13, 7)
+ - in features:
+   - "area" (18, 19)
+   - "liberties==3" (5)
+     - Black group has 3 liberties, white group has 4+ liberties
+   - "stone=pla/opp" (1, 2)
+
+
+Other notes:
+- Input channel 0 is all 1 always
+
+"""
+# %%
+
+model_layers = dict(model.outputs_by_layer + model.other_internal_outputs)
+print("Available model layers to look at:", list(model_layers.keys()))
+
+
+# Available model layers to look at: ['conv1', 'rconv1/trans1', 'rconv1/conv1', 'rconv1/trans2', 'rconv1/conv2', 'rconv1',
+# 'rconv2/trans1', 'rconv2/conv1', 'rconv2/trans2', 'rconv2/conv2', 'rconv2', 'rconv3/trans1', 'rconv3/conv1',
+# 'rconv3/trans2', 'rconv3/conv2', 'rconv3', 'rconv4/trans1', 'rconv4/conv1', 'rconv4/trans2', 'rconv4/conv2', 'rconv4',
+# 'rconv5/trans1', 'rconv5/conv1', 'rconv5/trans2', 'rconv5/conv2', 'rconv5', 'rconv6/trans1', 'rconv6/conv1a',
+# 'rconv6/conv1b', 'rconv6/trans2', 'rconv6/conv2', 'rconv6', 'rconv7/trans1', 'rconv7/conv1', 'rconv7/trans2',
+# 'rconv7/conv2', 'rconv7', 'rconv8/trans1', 'rconv8/conv1', 'rconv8/trans2', 'rconv8/conv2', 'rconv8', 'rconv9/trans1',
+# 'rconv9/conv1', 'rconv9/trans2', 'rconv9/conv2', 'rconv9', 'rconv10/trans1', 'rconv10/conv1', 'rconv10/trans2',
+# 'rconv10/conv2', 'rconv10', 'rconv11/trans1', 'rconv11/conv1a', 'rconv11/conv1b', 'rconv11/trans2', 'rconv11/conv2',
+# 'rconv11', 'rconv12/trans1', 'rconv12/conv1', 'rconv12/trans2', 'rconv12/conv2', 'rconv12', 'rconv13/trans1',
+# 'rconv13/conv1', 'rconv13/trans2', 'rconv13/conv2', 'rconv13', 'rconv14/trans1', 'rconv14/conv1', 'rconv14/trans2',
+# 'rconv14/conv2', 'rconv14', 'rconv15/trans1', 'rconv15/conv1', 'rconv15/trans2', 'rconv15/conv2', 'rconv15',
+# 'rconv16/trans1', 'rconv16/conv1a', 'rconv16/conv1b', 'rconv16/trans2', 'rconv16/conv2', 'rconv16', 'rconv17/trans1',
+# 'rconv17/conv1', 'rconv17/trans2', 'rconv17/conv2', 'rconv17', 'rconv18/trans1', 'rconv18/conv1', 'rconv18/trans2',
+# 'rconv18/conv2', 'rconv18', 'rconv19/trans1', 'rconv19/conv1', 'rconv19/trans2', 'rconv19/conv2', 'rconv19',
+# 'rconv20/trans1', 'rconv20/conv1', 'rconv20/trans2', 'rconv20/conv2', 'rconv20', 'rconv21/trans1', 'rconv21/conv1a',
+# 'rconv21/conv1b', 'rconv21/trans2', 'rconv21/conv2', 'rconv21', 'rconv22/trans1', 'rconv22/conv1', 'rconv22/trans2',
+# 'rconv22/conv2', 'rconv22', 'rconv23/trans1', 'rconv23/conv1', 'rconv23/trans2', 'rconv23/conv2', 'rconv23',
+# 'rconv24/trans1', 'rconv24/conv1', 'rconv24/trans2', 'rconv24/conv2', 'rconv24', 'rconv25/trans1', 'rconv25/conv1',
+# 'rconv25/trans2', 'rconv25/conv2', 'rconv25', 'rconv26/trans1', 'rconv26/conv1a', 'rconv26/conv1b', 'rconv26/trans2',
+# 'rconv26/conv2', 'rconv26', 'rconv27/trans1', 'rconv27/conv1', 'rconv27/trans2', 'rconv27/conv2', 'rconv27',
+# 'rconv28/trans1', 'rconv28/conv1', 'rconv28/trans2', 'rconv28/conv2', 'rconv28', 'rconv29/trans1', 'rconv29/conv1',
+# 'rconv29/trans2', 'rconv29/conv2', 'rconv29', 'rconv30/trans1', 'rconv30/conv1', 'rconv30/trans2', 'rconv30/conv2',
+# 'rconv30', 'rconv31/trans1', 'rconv31/conv1a', 'rconv31/conv1b', 'rconv31/trans2', 'rconv31/conv2', 'rconv31',
+# 'rconv32/trans1', 'rconv32/conv1', 'rconv32/trans2', 'rconv32/conv2', 'rconv32', 'rconv33/trans1', 'rconv33/conv1',
+# 'rconv33/trans2', 'rconv33/conv2', 'rconv33', 'rconv34/trans1', 'rconv34/conv1', 'rconv34/trans2', 'rconv34/conv2',
+# 'rconv34', 'rconv35/trans1', 'rconv35/conv1', 'rconv35/trans2', 'rconv35/conv2', 'rconv35', 'rconv36/trans1',
+# 'rconv36/conv1a', 'rconv36/conv1b', 'rconv36/trans2', 'rconv36/conv2', 'rconv36', 'rconv37/trans1', 'rconv37/conv1',
+# 'rconv37/trans2', 'rconv37/conv2', 'rconv37', 'rconv38/trans1', 'rconv38/conv1', 'rconv38/trans2', 'rconv38/conv2',
+# 'rconv38', 'rconv39/trans1', 'rconv39/conv1', 'rconv39/trans2', 'rconv39/conv2', 'rconv39', 'rconv40/trans1',
+# 'rconv40/conv1', 'rconv40/trans2', 'rconv40/conv2', 'rconv40', 'trunk', 'p1/intermediate_conv', 'g1/prenorm', 'g1',
+# 'g2', 'g3', 'p1', 'p2', 'pass', 'v1/prenorm', 'v1', 'vownership', 'vscoring', 'futurepos', 'seki']
+
+
+#   add_layer_visualizations("conv1",normalization_div=6)
+#   add_layer_visualizations("rconv1",normalization_div=14)
+#   add_layer_visualizations("rconv2",normalization_div=20)
+#   add_layer_visualizations("rconv3",normalization_div=26)
+#   add_layer_visualizations("rconv4",normalization_div=36)
+#   add_layer_visualizations("rconv5",normalization_div=40)
+#   add_layer_visualizations("rconv6",normalization_div=40)
+#   add_layer_visualizations("rconv7",normalization_div=44)
+#   add_layer_visualizations("rconv7/conv1a",normalization_div=12)
+#   add_layer_visualizations("rconv7/conv1b",normalization_div=12)
+#   add_layer_visualizations("rconv8",normalization_div=48)
+#   add_layer_visualizations("rconv9",normalization_div=52)
+#   add_layer_visualizations("rconv10",normalization_div=55)
+#   add_layer_visualizations("rconv11",normalization_div=58)
+#   add_layer_visualizations("rconv11/conv1a",normalization_div=12)
+#   add_layer_visualizations("rconv11/conv1b",normalization_div=12)
+#   add_layer_visualizations("rconv12",normalization_div=58)
+#   add_layer_visualizations("rconv13",normalization_div=64)
+#   add_layer_visualizations("rconv14",normalization_div=66)
+#   add_layer_visualizations("g1",normalization_div=6)
+#   add_layer_visualizations("p1",normalization_div=2)
+#   add_layer_visualizations("v1",normalization_div=4)
+
+
+# %% Look at the difference of the conv1 outputs
+
+conv1_cycle = sess.run(model_layers["rconv40"], feed_dict=gs_cycle.feed_dict())
+conv1_nocycle = sess.run(model_layers["rconv40"], feed_dict=gs_nocycle.feed_dict())
+
+fig = px.imshow(
+    (conv1_cycle - conv1_nocycle)[0, :, :, :],
+    facet_col=2,
+    facet_col_wrap=16,
+    facet_row_spacing=0.02,
+    color_continuous_scale="RdBu",
+    zmin=-5,
+    zmax=5,
+)
+fig.show()
+
+# %% Look at the difference of the policy outputs (p2)
+
+# trunk_cycle = sess.run(model_layers["trunk"], feed_dict=gs_cycle.feed_dict())
+# trunk_nocycle = sess.run(model_layers["trunk"], feed_dict=gs_nocycle.feed_dict())
+
+# fig = px.imshow(
+#     (trunk_cycle - trunk_nocycle)[0, :, :, :],
+#     facet_col=2,
+#     facet_col_wrap=16,
+#     facet_row_spacing=0.02,
+#     color_continuous_scale="RdBu",
+#     zmin=-10,
+#     zmax=10,
+# )
+# fig.show()
 
 # %%
 
-print("Available model layers to look at:", list(dict(model.outputs_by_layer).keys()))
+
+def make_quilt(pictures, facet_col=2, wrap=16):
+    assert len(pictures.shape) == 3
+    pictures = np.moveaxis(pictures, facet_col, 0)
+    quilted_img = np.zeros((pictures.shape[0], pictures.shape[1] + 1, pictures.shape[2] + 1))
+    quilted_img[:, :-1, :-1] = pictures
+    quilted_img = np.reshape(quilted_img, (-1, wrap, *quilted_img.shape[-2:]))
+    quilted_img = np.concatenate(tuple(np.concatenate(tuple(quilted_img), axis=1)), axis=1)
+    return quilted_img
+
+
+px.imshow(
+    make_quilt((conv1_cycle - conv1_nocycle)[0, :, :, :]),
+    color_continuous_scale="RdBu",
+    zmin=-100,
+    zmax=100,
+).show()
+
+# %% Make animation with all the layers
+
+layer_names = ["conv1"] + [f"rconv{i}" for i in range(1, 41)] + ["trunk"]
+layers_to_eval = [model_layers[name] for name in layer_names]
+
+layers_cycle = sess.run(layers_to_eval, feed_dict=gs_cycle.feed_dict())
+layers_nocycle = sess.run(layers_to_eval, feed_dict=gs_nocycle.feed_dict())
+
+
+def norm(img):
+    return img / np.abs(img).max()
+
+
+quilts = np.stack([norm(make_quilt((a - b)[0, :, :, :])) for (a, b) in zip(layers_cycle, layers_nocycle)])
+
+zmax = 1.0
+
+px.imshow(
+    plt.get_cmap("RdBu")(quilts / 2 + 0.5)[..., :3],
+    animation_frame=0,
+    color_continuous_scale="RdBu",
+    zmin=-zmax,
+    zmax=zmax,
+    binary_string=True,
+).show()
+
+
+# %% Do the same with the other non-cyclic image
+
+
+gs_nocycle_prime = sgf_to_gs(SGF.parse_file("connection_test_position_Bprime.sgf"))
+
+gs_nocycle_prime.feed_dict()[model.bin_inputs]
+gs_nocycle.feed_dict()[model.bin_inputs]
+
+# %% Make animation with all the layers
+
+layer_names = ["conv1"] + [f"rconv{i}" for i in range(1, 41)] + ["trunk"]
+layers_to_eval = [model_layers[name] for name in layer_names]
+
+layers_nocycle_prime = sess.run(layers_to_eval, feed_dict=gs_nocycle_prime.feed_dict())
+layers_nocycle = sess.run(layers_to_eval, feed_dict=gs_nocycle.feed_dict())
+
+quilts = np.stack([norm(make_quilt((a - b)[0, :, :, :])) for (a, b) in zip(layers_nocycle_prime, layers_nocycle)])
+
+zmax = 1.0
+
+px.imshow(
+    plt.get_cmap("RdBu")(quilts / 2 + 0.5)[..., :3],
+    animation_frame=0,
+    color_continuous_scale="RdBu",
+    zmin=-zmax,
+    zmax=zmax,
+    binary_string=True,
+).show()
+
+# %% Plot all the activations by themselves
+
+for layers in [layers_cycle, layers_nocycle, layers_nocycle_prime]:
+    quilts = np.stack([norm(make_quilt(a[0, :, :, :])) for a in layers_cycle])
+
+    print(
+        px.imshow(
+            plt.get_cmap("RdBu")(quilts / 2 + 0.5)[..., :3],
+            animation_frame=0,
+            color_continuous_scale="RdBu",
+            zmin=-zmax,
+            zmax=zmax,
+            binary_string=True,
+        ).show()
+    )
