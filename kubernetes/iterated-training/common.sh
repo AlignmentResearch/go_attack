cfa343abe6c608a7512728d9c79d3e25618124c9
Tom Tseng <tom.hm.tseng@gmail.com>
2023-04-02 01:22:57 -0400

    common fixes

--- kubernetes/iterated-training/common.sh
+++ kubernetes/iterated-training/common.sh
@@ -4,7 +4,7 @@
 # Returns exit code 0 if the process with PID $1 is still running, otherwise
 # returns a non-zero exit code.
 is_process_running() {
-  PID=$1
+  local PID=$1
   ps -p "$PID" > /dev/null
   return
 }
@@ -12,7 +12,7 @@ is_process_running() {
 # Exits the script if the process with PID $1 has exited with an error. Returns
 # exit code 0 if the process is still running or exited without an error.
 assert_process_has_not_errored() {
-  PID=$1
+  local PID=$1
   if is_process_running "$PID"; then
     return 0
   fi
@@ -26,20 +26,20 @@ assert_process_has_not_errored() {
 # finished, the function returns exit code 0, otherwise it returns a non-zero
 # exit code.
 is_curriculum_complete() {
-  CURRICULUM_LOG_DIR="$1"/selfplay
+  local CURRICULUM_LOG_DIR="$1"/selfplay
   if [ ! -d "$CURRICULUM_LOG_DIR" ]; then
     false
     return
   fi
   # shellcheck disable=SC2010
-  CURRICULUM_LOGS=$(ls -v "$CURRICULUM_LOG_DIR" | grep "^curriculum-.*\.log")
+  local CURRICULUM_LOGS=$(ls -v "$CURRICULUM_LOG_DIR" | grep "^curriculum-.*\.log")
   if [ -z "$CURRICULUM_LOGS" ]; then
     false
     return
   fi
-  LATEST_CURRICULUM_LOG=$(echo "$CURRICULUM_LOGS" | tail --lines 1)
+  local LATEST_CURRICULUM_LOG=$(echo "$CURRICULUM_LOGS" | tail --lines 1)
   LATEST_CURRICULUM_LOG="$CURRICULUM_LOG_DIR"/"$LATEST_CURRICULUM_LOG"
-  LAST_LOG_LINE=$(tail --lines 1 "$LATEST_CURRICULUM_LOG")
+  local LAST_LOG_LINE=$(tail --lines 1 "$LATEST_CURRICULUM_LOG")
   [[ $LAST_LOG_LINE == "Curriculum finished"* ]]
   return
 }
@@ -48,18 +48,18 @@ is_curriculum_complete() {
 # $1 is the output directory for the victimplay run, and the remaining arguments
 # are the command to run.
 run_until_curriculum_done() {
-  RUN_DIR=$1
+  local RUN_DIR=$1
   shift
-  COMMAND="$@"
+  local COMMAND="$@"
 
   # To make sure we also kill any child processes when we run the command, we
   # use setsid to give the command its own process group. Then we can kill that
   # process group to kill the command along with all its child processes.
   setsid "$COMMAND" &
-  COMMAND_PID=$1
+  local COMMAND_PID=$1
 
-  while ! is_curriculum_complete "$ITERATION_DIR"; do
-    assert_process_has_not_errored "$VICTIMPLAY_PID"
+  while ! is_curriculum_complete "$RUN_DIR"; do
+    assert_process_has_not_errored "$COMMAND_PID"
     sleep 10
   done
   kill -- -"$COMMAND_PID"
