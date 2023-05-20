1a1ea881f1aecaddbc6f7cc2b74aeeade8c84366
Tony Wang <twang6@mit.edu>
2023-05-20 15:13:47 -0700

    Fix ctl wrapper and adjust batch size

--- kubernetes/ctl_job_run_wrapper.py
+++ kubernetes/ctl_job_run_wrapper.py
@@ -140,7 +140,7 @@ class CtlWrapperArgs:
                 client.containers.run(
                     image=container,
                     command=command,
-                    name=f"{self.name}-r{ridx}",
+                    labels={"job-name": f"{self.name}-r{ridx}"},
                     detach=True,
                     # Volumes
                     volumes={
