10c844f0d6d1005d214c8342b08c6e74076408e0
Tom Tseng <tom.hm.tseng@gmail.com>
2023-06-13 14:24:26 -0700

    use KataGo-raw gatekeeper

--- kubernetes/gatekeeper.sh
+++ kubernetes/gatekeeper.sh
@@ -6,8 +6,7 @@ TRAINING_DIR=/"$VOLUME_NAME"/victimplay/"$RUN_NAME"
 mkdir -p "$TRAINING_DIR"
 
 /engines/KataGo-custom/cpp/katago gatekeeper \
-  -config /go_attack/configs/amcts/gatekeeper.cfg \
-  -config /go_attack/configs/compute/1gpu.cfg \
+  -config /go_attack/engines/KataGo-raw/cpp/configs/training/gatekeeper1.cfg \
   -accepted-models-dir "$TRAINING_DIR/models"  \
   -rejected-models-dir "$TRAINING_DIR/rejectedmodels" \
   -test-models-dir "$TRAINING_DIR/modelstobetested" \
