3fbc7ca64c9c075d56f3f41d6dbf33fa1f3bbfe7
Tom Tseng <tom.hm.tseng@gmail.com>
2023-07-13 15:52:26 -0700

    victim=finetuned-cp505; preseed with cyclic-adv data; more 19x19 games

--- kubernetes/launch-job.sh
+++ kubernetes/launch-job.sh
@@ -182,6 +182,7 @@ if [ -n "${USE_ITERATED_TRAINING:-}" ]; then
   CURRICULUM_CMD="/go_attack/kubernetes/iterated-training/curriculum.sh $RUN_NAME $VOLUME_NAME $CURRICULUM $ALTERNATE_CURRICULUM $ALTERNATE_ITERATION_FIRST"
 else
   VICTIMPLAY_CMD+=" $VICTIMPLAY_FLAGS $RUN_NAME $VOLUME_NAME"
+  # EVALUATE_LOOP_CMD="/go_attack/kubernetes/evaluate_loop_custom.sh $PREDICTOR_FLAG /$VOLUME_NAME/victimplay/$RUN_NAME /$VOLUME_NAME/victimplay/$RUN_NAME/eval"
   EVALUATE_LOOP_CMD="/engines/KataGo-custom/cpp/evaluate_loop.sh $PREDICTOR_FLAG /$VOLUME_NAME/victimplay/$RUN_NAME /$VOLUME_NAME/victimplay/$RUN_NAME/eval"
   TRAIN_CMD="/go_attack/kubernetes/train.sh $TRAIN_FLAGS $RUN_NAME $VOLUME_NAME $LR_SCALE"
   SHUFFLE_AND_EXPORT_CMD="/go_attack/kubernetes/shuffle-and-export.sh $RUN_NAME $RUN_NAME $VOLUME_NAME $USE_GATING"
